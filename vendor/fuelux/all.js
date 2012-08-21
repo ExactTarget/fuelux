04d8dd5138ffbc5dc2f9c939721cc71e08714018
Adam Alexander <adamalex@gmail.com>
2012-08-21 19:28:00 -0400

    Update FuelUX

--- vendor/fuelux/all.js
+++ vendor/fuelux/all.js
@@ -2407,6 +2407,182 @@ define('fuelux/search',['require','jquery'],function(require) {
 });
 
 /*
+ * FuelUX Spinner
+ * https://github.com/ExactTarget/fuelux
+ *
+ * Copyright (c) 2012 ExactTarget
+ * Licensed under the MIT license.
+ */
+
+define('fuelux/spinner',['require','jquery'],function(require) {
+
+	var $ = require('jquery');
+
+
+	// SPINNER CONSTRUCTOR AND PROTOTYPE
+
+	var Spinner = function (element, options) {
+		this.$element = $(element);
+		this.options = $.extend({}, $.fn.spinner.defaults, options);
+		this.$input = this.$element.find('.spinner-input');
+		this.$element.on('keyup', this.$input, $.proxy(this.change, this));
+
+		if (this.options.hold) {
+			this.$element.on('mousedown', '.spinner-up', $.proxy(function() { this.startSpin(true); } , this));
+			this.$element.on('mouseup', '.spinner-up, .spinner-down', $.proxy(this.stopSpin, this));
+			this.$element.on('mouseout', '.spinner-up, .spinner-down', $.proxy(this.stopSpin, this));
+			this.$element.on('mousedown', '.spinner-down', $.proxy(function() {this.startSpin(false);} , this));
+		} else {
+			this.$element.on('click', '.spinner-up', $.proxy(function() { this.increment(true); } , this));
+			this.$element.on('click', '.spinner-down', $.proxy(function() { this.increment(false); }, this));
+		}
+
+		this.switches = {
+			count: 1,
+			enabled: true
+		};
+
+		if (this.options.speed === 'medium') {
+			this.switches.speed = 300;
+		} else if (this.options.speed === 'fast') {
+			this.switches.speed = 100;
+		} else {
+			this.switches.speed = 500;
+		}
+
+		this.render();
+
+		if (this.options.disabled) {
+			this.disable();
+		}
+	};
+
+	Spinner.prototype = {
+		constructor: Spinner,
+
+		render: function () {
+			this.$input.val(this.options.value);
+			this.$input.attr('maxlength',(this.options.max + '').split('').length);
+		},
+
+		change: function () {
+			this.options.value = this.$input.val()/1;
+			this.$element.trigger('change');
+		},
+
+		stopSpin: function () {
+			clearTimeout(this.switches.timeout);
+			this.switches.count = 1;
+			this.$element.trigger('change');
+		},
+
+		startSpin: function (type) {
+
+			if (!this.options.disabled) {
+				var divisor = this.switches.count;
+
+				if (divisor === 1) {
+					this.increment(type);
+					divisor = 1;
+				} else if (divisor < 3){
+					divisor = 1.5;
+				} else if (divisor < 8){
+					divisor = 2.5;
+				} else {
+					divisor = 4;
+				}
+
+				this.switches.timeout = setTimeout($.proxy(function() {this.iterator(type);} ,this),this.switches.speed/divisor);
+				this.switches.count++;
+			}
+		},
+
+		iterator: function (type) {
+			this.increment(type);
+			this.startSpin(type);
+		},
+
+		increment: function (dir) {
+			var curValue = this.options.value;
+			var limValue = dir ? this.options.max : this.options.min;
+
+			if ((dir ? curValue < limValue : curValue > limValue)) {
+				var newVal = curValue + (dir ? 1 : -1) * this.options.increment;
+
+				if (dir ? newVal > limValue : newVal < limValue) {
+					this.value(limValue);
+				} else {
+					this.value(newVal);
+				}
+			}
+		},
+
+		value: function (value) {
+			if (value) {
+				this.options.value = value;
+				this.$input.val(value);
+				return this;
+			} else {
+				return this.options.value;
+			}
+		},
+
+		disable: function () {
+			this.options.disabled = true;
+			this.$input.attr('disabled','');
+			this.$element.find('button').addClass('disabled');
+		},
+
+		enable: function () {
+			this.options.disabled = false;
+			this.$input.removeAttr("disabled");
+			this.$element.find('button').removeClass('disabled');
+		}
+	};
+
+
+	// SPINNER PLUGIN DEFINITION
+
+	$.fn.spinner = function (option,value) {
+		var methodReturn;
+
+		var $set = this.each(function () {
+			var $this = $(this);
+			var data = $this.data('spinner');
+			var options = typeof option === 'object' && option;
+
+			if (!data) $this.data('spinner', (data = new Spinner(this, options)));
+			if (typeof option === 'string') methodReturn = data[option](value);
+		});
+
+		return (methodReturn === undefined) ? $set : methodReturn;
+	};
+
+	$.fn.spinner.defaults = {
+		value: 1,
+		min: 1,
+		max: 999,
+		increment: 1,
+		hold: true,
+		speed: 'medium',
+		disabled: false
+	};
+
+	$.fn.spinner.Constructor = Spinner;
+
+
+	// SPINNER DATA-API
+
+	$(function () {
+		$('body').on('mousedown.spinner.data-api', '.spinner', function (e) {
+			var $this = $(this);
+			if ($this.data('.spinner')) return;
+			$this.spinner($this.data());
+		});
+	});
+
+});
+/*
  * FuelUX
  * https://github.com/ExactTarget/fuelux
  *
@@ -2414,7 +2590,7 @@ define('fuelux/search',['require','jquery'],function(require) {
  * Licensed under the MIT license.
  */
 
-define('fuelux/all',['require','jquery','bootstrap/bootstrap-alert','bootstrap/bootstrap-button','bootstrap/bootstrap-carousel','bootstrap/bootstrap-collapse','bootstrap/bootstrap-dropdown','bootstrap/bootstrap-modal','bootstrap/bootstrap-popover','bootstrap/bootstrap-scrollspy','bootstrap/bootstrap-tab','bootstrap/bootstrap-tooltip','bootstrap/bootstrap-transition','bootstrap/bootstrap-typeahead','fuelux/combobox','fuelux/datagrid','fuelux/pillbox','fuelux/search'],function (require) {
+define('fuelux/all',['require','jquery','bootstrap/bootstrap-alert','bootstrap/bootstrap-button','bootstrap/bootstrap-carousel','bootstrap/bootstrap-collapse','bootstrap/bootstrap-dropdown','bootstrap/bootstrap-modal','bootstrap/bootstrap-popover','bootstrap/bootstrap-scrollspy','bootstrap/bootstrap-tab','bootstrap/bootstrap-tooltip','bootstrap/bootstrap-transition','bootstrap/bootstrap-typeahead','fuelux/combobox','fuelux/datagrid','fuelux/pillbox','fuelux/search','fuelux/spinner'],function (require) {
 	require('jquery');
 	require('bootstrap/bootstrap-alert');
 	require('bootstrap/bootstrap-button');
@@ -2432,4 +2608,5 @@ define('fuelux/all',['require','jquery','bootstrap/bootstrap-alert','bootstrap/b
 	require('fuelux/datagrid');
 	require('fuelux/pillbox');
 	require('fuelux/search');
+	require('fuelux/spinner');
 });
