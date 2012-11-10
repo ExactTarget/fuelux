a478bb3f3b25a377ebf272bb55d0b064ec998c16
Adam Alexander <adamalex@gmail.com>
2012-11-10 15:58:17 -0500

    Eat keydown event on search input (fixes #55)

--- dist/all.js
+++ dist/all.js
@@ -2533,6 +2533,7 @@ define('fuelux/search',['require','jquery'],function(require) {
 		this.$element = $(element);
 		this.options = $.extend({}, $.fn.search.defaults, options);
 		this.$element.find('button').on('click', $.proxy(this.buttonclicked, this));
+		this.$input = this.$element.find('input').on('keydown', $.proxy(this.keypress, this));
 		this.$input = this.$element.find('input').on('keyup', $.proxy(this.keypressed, this));
 		this.$icon = this.$element.find('i');
 		this.activeSearch = '';
@@ -2570,6 +2571,12 @@ define('fuelux/search',['require','jquery'],function(require) {
 			e.preventDefault();
 			if ($(e.currentTarget).is('.disabled, :disabled')) return;
 			this.action();
+		},
+
+		keypress: function (e) {
+			if (e.which === 13) {
+				e.preventDefault();
+			}
 		},
 
 		keypressed: function (e) {
