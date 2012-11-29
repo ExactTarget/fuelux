7196d8576823e483b6328677fb4c2651483aad2f
Adam Alexander <aalexander@exacttarget.com>
2012-11-29 07:44:44 -0500

    Update formatting

--- dist/all.js
+++ dist/all.js
@@ -2161,182 +2161,182 @@
  * Licensed under the MIT license.
  */
 
-define('fuelux/combobox',['require','jquery'],function(require) {
+define('fuelux/combobox',['require','jquery'],function (require) {
 
-    var $ = require('jquery');
+	var $ = require('jquery');
 
-    // COMBOBOX CONSTRUCTOR AND PROTOTYPE
+	// COMBOBOX CONSTRUCTOR AND PROTOTYPE
 
-    var Combobox = function (element, options) {
-        this.$element = $(element);
-        this.options = $.extend({}, $.fn.combobox.defaults, options);
-        this.$element.on('click', 'a', $.proxy(this.itemclicked, this));
-        this.$element.on('change', 'input', $.proxy(this.inputchanged, this));
-        this.$input = this.$element.find('input');
-        this.$button = this.$element.find('.btn');
+	var Combobox = function (element, options) {
+		this.$element = $(element);
+		this.options = $.extend({}, $.fn.combobox.defaults, options);
+		this.$element.on('click', 'a', $.proxy(this.itemclicked, this));
+		this.$element.on('change', 'input', $.proxy(this.inputchanged, this));
+		this.$input = this.$element.find('input');
+		this.$button = this.$element.find('.btn');
 
-        // custom case-insensitive match expression
-        $.extend($.expr[':'], {
-            match: function(elem, index, match) {
-                var matchtext = (match && 3 in match) ? match[3] : '';
-                return ((elem.textContent || elem.innerText || $(elem).text() || "").toLowerCase() === matchtext.toLowerCase());
-            }
-        });
+		// custom case-insensitive match expression
+		$.extend($.expr[':'], {
+			match: function (elem, index, match) {
+				var matchtext = (match && 3 in match) ? match[3] : '';
+				return ((elem.textContent || elem.innerText || $(elem).text() || "").toLowerCase() === matchtext.toLowerCase());
+			}
+		});
 
-        // set default selection
-        this.setDefaultSelection();
-    };
+		// set default selection
+		this.setDefaultSelection();
+	};
 
-    Combobox.prototype = {
+	Combobox.prototype = {
 
-        constructor: Combobox,
+		constructor: Combobox,
 
-        select: function (val) {
-            this.$input.val(val).change();
-            return this;
-        },
+		select: function (val) {
+			this.$input.val(val).change();
+			return this;
+		},
 
-        selectedItem: function() {
-            var item = this.$selectedItem;
-            var data = {};
+		selectedItem: function () {
+			var item = this.$selectedItem;
+			var data = {};
 
-            if (item) {
-                var txt = this.$selectedItem.text();
-                data = $.extend({ text: txt }, this.$selectedItem.data());
-            }
-            else {
-                data = { text:this.$input.val()};
-            }
+			if (item) {
+				var txt = this.$selectedItem.text();
+				data = $.extend({ text: txt }, this.$selectedItem.data());
+			}
+			else {
+				data = { text: this.$input.val()};
+			}
 
-            return data;
-        },
+			return data;
+		},
 
-        selectByText: function(text) {
-            var selector = 'li:match(' + text + ')';
-            this.selectBySelector(selector);
-        },
+		selectByText: function (text) {
+			var selector = 'li:match(' + text + ')';
+			this.selectBySelector(selector);
+		},
 
-        selectByValue: function(value) {
-            var selector = 'li[data-value=' + value + ']';
-            this.selectBySelector(selector);
-        },
+		selectByValue: function (value) {
+			var selector = 'li[data-value=' + value + ']';
+			this.selectBySelector(selector);
+		},
 
-        selectByIndex: function(index) {
-            // zero-based index
-            var selector = 'li:eq(' + index + ')';
-            this.selectBySelector(selector);
-        },
+		selectByIndex: function (index) {
+			// zero-based index
+			var selector = 'li:eq(' + index + ')';
+			this.selectBySelector(selector);
+		},
 
-        selectBySelector: function(selector) {
-            var $item = this.$element.find(selector);
+		selectBySelector: function (selector) {
+			var $item = this.$element.find(selector);
 
-            if (typeof $item[0] !== 'undefined') {
-                this.$selectedItem = $item;
-                this.$input.val(this.$selectedItem.text());
-            }
-            else {
-                this.$selectedItem = null;
-            }
-        },
+			if (typeof $item[0] !== 'undefined') {
+				this.$selectedItem = $item;
+				this.$input.val(this.$selectedItem.text());
+			}
+			else {
+				this.$selectedItem = null;
+			}
+		},
 
-        setDefaultSelection: function() {
-            var selector = 'li[data-selected=true]:first';
-            var item = this.$element.find(selector);
-            if(item.length === 0) {
-                // select first item
-                this.selectByIndex(0);
-            }
-            else {
-                // select by data-attribute
-                this.selectBySelector(selector);
-                item.removeData('selected');
-                item.removeAttr('data-selected');
-            }
-        },
+		setDefaultSelection: function () {
+			var selector = 'li[data-selected=true]:first';
+			var item = this.$element.find(selector);
+			if (item.length === 0) {
+				// select first item
+				this.selectByIndex(0);
+			}
+			else {
+				// select by data-attribute
+				this.selectBySelector(selector);
+				item.removeData('selected');
+				item.removeAttr('data-selected');
+			}
+		},
 
-        enable: function() {
-            this.$input.removeAttr('disabled');
-            this.$button.removeClass('disabled');
-        },
+		enable: function () {
+			this.$input.removeAttr('disabled');
+			this.$button.removeClass('disabled');
+		},
 
-        disable: function() {
-            this.$input.attr('disabled', true);
-            this.$button.addClass('disabled');
-        },
+		disable: function () {
+			this.$input.attr('disabled', true);
+			this.$button.addClass('disabled');
+		},
 
-        itemclicked: function (e) {
-            this.$selectedItem = $(e.target).parent();
-            this.$input.val(this.$selectedItem.text());
+		itemclicked: function (e) {
+			this.$selectedItem = $(e.target).parent();
+			this.$input.val(this.$selectedItem.text());
 
-            // pass object including text and any data-attributes
-            // to onchange event
-            var data = this.selectedItem();
+			// pass object including text and any data-attributes
+			// to onchange event
+			var data = this.selectedItem();
 
-            // trigger changed event
-            this.$element.trigger('changed', data);
+			// trigger changed event
+			this.$element.trigger('changed', data);
 
-            e.preventDefault();
-        },
+			e.preventDefault();
+		},
 
-        inputchanged: function(e) {
-            var val = $(e.target).val();
-            this.selectByText(val);
+		inputchanged: function (e) {
+			var val = $(e.target).val();
+			this.selectByText(val);
 
-            // find match based on input
-            // if no match, pass the input value
-            var data = this.selectedItem();
-            if(data.text.length === 0) {
-                data = { text: val };
-            }
+			// find match based on input
+			// if no match, pass the input value
+			var data = this.selectedItem();
+			if (data.text.length === 0) {
+				data = { text: val };
+			}
 
-            // trigger changed event
-            this.$element.trigger('changed', data);
+			// trigger changed event
+			this.$element.trigger('changed', data);
 
-        }
+		}
 
-    };
-
-
-    // COMBOBOX PLUGIN DEFINITION
-
-    $.fn.combobox = function (option, value) {
-        var methodReturn;
-
-        var $set = this.each(function () {
-            var $this = $(this);
-            var data = $this.data('combobox');
-            var options = typeof option === 'object' && option;
-
-            if (!data) $this.data('combobox', (data = new Combobox(this, options)));
-            if (typeof option === 'string') methodReturn = data[option](value);
-        });
-
-        return (methodReturn === undefined) ? $set : methodReturn;
-    };
-
-    $.fn.combobox.defaults = {};
-
-    $.fn.combobox.Constructor = Combobox;
+	};
 
 
-    // COMBOBOX DATA-API
+	// COMBOBOX PLUGIN DEFINITION
 
-    $(function () {
+	$.fn.combobox = function (option, value) {
+		var methodReturn;
 
-        $(window).on('load', function () {
-            $('.combobox').each(function () {
-                var $this = $(this);
-                if ($this.data('combobox')) return;
-                $this.combobox($this.data());
-            });
-        });
+		var $set = this.each(function () {
+			var $this = $(this);
+			var data = $this.data('combobox');
+			var options = typeof option === 'object' && option;
 
-        $('body').on('mousedown.combobox.data-api', '.combobox', function (e) {
-            var $this = $(this);
-            if ($this.data('combobox')) return;
-            $this.combobox($this.data());
-        });
-    });
+			if (!data) $this.data('combobox', (data = new Combobox(this, options)));
+			if (typeof option === 'string') methodReturn = data[option](value);
+		});
+
+		return (methodReturn === undefined) ? $set : methodReturn;
+	};
+
+	$.fn.combobox.defaults = {};
+
+	$.fn.combobox.Constructor = Combobox;
+
+
+	// COMBOBOX DATA-API
+
+	$(function () {
+
+		$(window).on('load', function () {
+			$('.combobox').each(function () {
+				var $this = $(this);
+				if ($this.data('combobox')) return;
+				$this.combobox($this.data());
+			});
+		});
+
+		$('body').on('mousedown.combobox.data-api', '.combobox', function (e) {
+			var $this = $(this);
+			if ($this.data('combobox')) return;
+			$this.combobox($this.data());
+		});
+	});
 
 });
 
--- test/combobox-test.js
+++ test/combobox-test.js
@@ -2,115 +2,115 @@
 /*global start:false, stop:false ok:false, equal:false, notEqual:false, deepEqual:false*/
 /*global notDeepEqual:false, strictEqual:false, notStrictEqual:false, raises:false*/
 
-require(['jquery', 'fuelux/combobox'], function($) {
+require(['jquery', 'fuelux/combobox'], function ($) {
 
-    $.extend($.expr[':'], {
-        match: function(elem, index, match) {
-            var matchtext = (match && 3 in match) ? match[3] : '';
-            return ((elem.textContent || elem.innerText || $(elem).text() || "").toLowerCase() === matchtext.toLowerCase());
-        }
-    });
+	$.extend($.expr[':'], {
+		match: function (elem, index, match) {
+			var matchtext = (match && 3 in match) ? match[3] : '';
+			return ((elem.textContent || elem.innerText || $(elem).text() || "").toLowerCase() === matchtext.toLowerCase());
+		}
+	});
 
-    var html = '<div class="input-append dropdown combobox">' +
-        '<input class="span2" type="text"><button class="btn" data-toggle="dropdown"><i class="caret"></i></button>' +
-        '<ul class="dropdown-menu">' +
-        '<li data-value="1"><a href="#">One</a></li>' +
-        '<li data-value="2"><a href="#">Two</a></li>' +
-        '<li data-value="3" data-selected="true"><a href="#">Three</a></li>' +
-        '<li data-value="4" data-foo="bar" data-fizz="buzz"><a href="#">Four</a></li>' +
-        '</ul>' +
-        '</div>';
+	var html = '<div class="input-append dropdown combobox">' +
+		'<input class="span2" type="text"><button class="btn" data-toggle="dropdown"><i class="caret"></i></button>' +
+		'<ul class="dropdown-menu">' +
+		'<li data-value="1"><a href="#">One</a></li>' +
+		'<li data-value="2"><a href="#">Two</a></li>' +
+		'<li data-value="3" data-selected="true"><a href="#">Three</a></li>' +
+		'<li data-value="4" data-foo="bar" data-fizz="buzz"><a href="#">Four</a></li>' +
+		'</ul>' +
+		'</div>';
 
-    module("Fuel UX combobox");
+	module("Fuel UX combobox");
 
-    test("should be defined on jquery object", function () {
-        ok($(document.body).combobox, 'combobox method is defined');
-    });
+	test("should be defined on jquery object", function () {
+		ok($(document.body).combobox, 'combobox method is defined');
+	});
 
-    test("should return element", function () {
-        ok($(document.body).combobox()[0] === document.body, 'document.body returned');
-    });
+	test("should return element", function () {
+		ok($(document.body).combobox()[0] === document.body, 'document.body returned');
+	});
 
-    test("should set disabled state", function() {
-        var $combobox = $(html).combobox();
-        $combobox.combobox('disable');
-        equal($combobox.find('.btn').hasClass('disabled'), true, 'element disabled');
-    });
+	test("should set disabled state", function () {
+		var $combobox = $(html).combobox();
+		$combobox.combobox('disable');
+		equal($combobox.find('.btn').hasClass('disabled'), true, 'element disabled');
+	});
 
-    test("should set enabled state", function() {
-        var $combobox = $(html).combobox();
-        $combobox.combobox('disable');
-        $combobox.combobox('enable');
-        equal($combobox.find('.btn').hasClass('disabled'), false, 'element enabled');
-    });
+	test("should set enabled state", function () {
+		var $combobox = $(html).combobox();
+		$combobox.combobox('disable');
+		$combobox.combobox('enable');
+		equal($combobox.find('.btn').hasClass('disabled'), false, 'element enabled');
+	});
 
-    test("should set default selection", function() {
-        // should be "Three" based on the data-selected attribute
-        var $combobox = $(html).combobox();
-        var item = $combobox.combobox('selectedItem');
-        var expectedItem = { text:'Three', value:3 };
-        deepEqual(item, expectedItem, 'default item selected');
-    });
+	test("should set default selection", function () {
+		// should be "Three" based on the data-selected attribute
+		var $combobox = $(html).combobox();
+		var item = $combobox.combobox('selectedItem');
+		var expectedItem = { text: 'Three', value: 3 };
+		deepEqual(item, expectedItem, 'default item selected');
+	});
 
-    test("should select by index", function() {
-        var $combobox = $(html).combobox();
-        $combobox.combobox('selectByIndex', 0);
+	test("should select by index", function () {
+		var $combobox = $(html).combobox();
+		$combobox.combobox('selectByIndex', 0);
 
-        var item = $combobox.combobox('selectedItem');
-        var expectedItem = { text:'One', value:1 };
-        deepEqual(item, expectedItem, 'item selected');
-    });
+		var item = $combobox.combobox('selectedItem');
+		var expectedItem = { text: 'One', value: 1 };
+		deepEqual(item, expectedItem, 'item selected');
+	});
 
-    test("should select by value", function() {
-        var $combobox = $(html).combobox();
-        $combobox.combobox('selectByValue', 2);
+	test("should select by value", function () {
+		var $combobox = $(html).combobox();
+		$combobox.combobox('selectByValue', 2);
 
-        var item = $combobox.combobox('selectedItem');
-        var expectedItem = { text:'Two', value:2 };
-        deepEqual(item, expectedItem, 'item selected');
-    });
+		var item = $combobox.combobox('selectedItem');
+		var expectedItem = { text: 'Two', value: 2 };
+		deepEqual(item, expectedItem, 'item selected');
+	});
 
-    test("should select by selector", function() {
-        var $combobox = $(html).combobox();
-        $combobox.combobox('selectBySelector', 'li[data-fizz=buzz]');
+	test("should select by selector", function () {
+		var $combobox = $(html).combobox();
+		$combobox.combobox('selectBySelector', 'li[data-fizz=buzz]');
 
-        var item = $combobox.combobox('selectedItem');
-        var expectedItem = { text:'Four', value:4, foo:'bar', fizz:'buzz' };
-        deepEqual(item, expectedItem, 'item selected');
-    });
+		var item = $combobox.combobox('selectedItem');
+		var expectedItem = { text: 'Four', value: 4, foo: 'bar', fizz: 'buzz' };
+		deepEqual(item, expectedItem, 'item selected');
+	});
 
-    test("should fire changed event - item selected", function() {
-        var eventFired = false;
-        var selectedText = '';
-        var selectedValue = '';
+	test("should fire changed event - item selected", function () {
+		var eventFired = false;
+		var selectedText = '';
+		var selectedValue = '';
 
-        var $combobox = $(html).combobox().on('changed', function (evt, data) {
-            eventFired = true;
-            selectedText = data.text;
-            selectedValue = data.value;
-        });
+		var $combobox = $(html).combobox().on('changed', function (evt, data) {
+			eventFired = true;
+			selectedText = data.text;
+			selectedValue = data.value;
+		});
 
-        // simulate changed event
-        $combobox.find('a:first').click();
+		// simulate changed event
+		$combobox.find('a:first').click();
 
-        equal(eventFired, true, 'changed event fired');
-        equal(selectedText, 'One', 'text passed in from changed event');
-        equal(selectedValue, 1, 'value passed in from changed event');
-    });
+		equal(eventFired, true, 'changed event fired');
+		equal(selectedText, 'One', 'text passed in from changed event');
+		equal(selectedValue, 1, 'value passed in from changed event');
+	});
 
-    test("should fire changed event - input changed", function() {
-        var eventFired = false;
-        var selectedText = '';
+	test("should fire changed event - input changed", function () {
+		var eventFired = false;
+		var selectedText = '';
 
-        var $combobox = $(html).combobox().on('changed', function (evt, data) {
-            eventFired = true;
-            selectedText = data.text;
-        });
+		var $combobox = $(html).combobox().on('changed', function (evt, data) {
+			eventFired = true;
+			selectedText = data.text;
+		});
 
-        $combobox.find('input').val('Seven').change();
+		$combobox.find('input').val('Seven').change();
 
-        equal(eventFired, true, 'changed event fired');
-        equal(selectedText, 'Seven', 'text passed in from changed event');
-    });
+		equal(eventFired, true, 'changed event fired');
+		equal(selectedText, 'Seven', 'text passed in from changed event');
+	});
 
 });
