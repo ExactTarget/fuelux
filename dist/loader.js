79c434402f91d5975a8a1aa8a6f5693496518772
Adam Alexander <aalexander@exacttarget.com>
2012-12-07 09:46:02 -0500

    Restore jQuery 1.7+ compatibility

--- dist/loader.js
+++ dist/loader.js
@@ -2617,11 +2617,20 @@ define('fuelux/util',['require','jquery'],function (require) {
 	var $ = require('jquery');
 
 	// custom case-insensitive match expression
-	$.expr.pseudos.fuelTextExactCI = $.expr.createPseudo(function (arg) {
-		return function (elem) {
-			return (elem.textContent || elem.innerText || $(elem).text() || '').toLowerCase() === arg.toLowerCase();
+	function fuelTextExactCI(elem, text) {
+		return (elem.textContent || elem.innerText || $(elem).text() || '').toLowerCase() === (text || '').toLowerCase();
+	}
+
+	$.expr[':'].fuelTextExactCI = $.expr.createPseudo ?
+		$.expr.createPseudo(function (text) {
+			return function (elem) {
+				return fuelTextExactCI(elem, text);
+			};
+		}) :
+		function (elem, i, match) {
+			return fuelTextExactCI(elem, match[3]);
 		};
-	});
+
 });
 /*
  * Fuel UX Combobox
