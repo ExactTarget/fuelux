a6dab9cf38c8aeb1d4f2c005263236cf137585c1
Adam Alexander <adamalex@gmail.com>
2013-05-16 21:08:20 -0400

    Fix config for updated sauce plugin

--- Gruntfile.js
+++ Gruntfile.js
@@ -49,20 +49,22 @@ module.exports = function (grunt) {
 		},
 		'saucelabs-qunit': {
 			all: {
-				urls: '<%= testUrls %>',
-				concurrency: '3',
-				browsers: [
-					{ browserName: 'internet explorer', platform: 'Windows 2012', version: '10' },
-					{ browserName: 'internet explorer', platform: 'Windows 2008', version: '9' },
-					{ browserName: 'internet explorer', platform: 'Windows 2008', version: '8' },
-					{ browserName: 'firefox', platform: 'Windows 2008', version: '19' },
-					{ browserName: 'firefox', platform: 'Mac 10.6', version: '19' },
-					{ browserName: 'safari', platform: 'Mac 10.8', version: '6' },
-					{ browserName: 'chrome', platform: 'Windows 2008' },
-					{ browserName: 'chrome', platform: 'Mac 10.8' },
-					{ browserName: 'iphone', platform: 'Mac 10.8', version: '6' },
-					{ browserName: 'ipad', platform: 'Mac 10.8', version: '6' }
-				]
+				options: {
+					urls: '<%= testUrls %>',
+					concurrency: '3',
+					browsers: [
+						{ browserName: 'internet explorer', platform: 'Windows 2012', version: '10' },
+						{ browserName: 'internet explorer', platform: 'Windows 2008', version: '9' },
+						{ browserName: 'internet explorer', platform: 'Windows 2008', version: '8' },
+						{ browserName: 'firefox', platform: 'Windows 2008', version: '19' },
+						{ browserName: 'firefox', platform: 'Mac 10.6', version: '19' },
+						{ browserName: 'safari', platform: 'Mac 10.8', version: '6' },
+						{ browserName: 'chrome', platform: 'Windows 2008' },
+						{ browserName: 'chrome', platform: 'Mac 10.8' },
+						{ browserName: 'iphone', platform: 'Mac 10.8', version: '6' },
+						{ browserName: 'ipad', platform: 'Mac 10.8', version: '6' }
+					]
+				}
 			}
 		},
 		watch: {
