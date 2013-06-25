ccc35e7b351ee819f574e11add22079d0275f13a
Ryan Moore <rmoore@et.local>
2013-06-25 16:08:00 -0400

    issue-129

--- src/pillbox.js
+++ src/pillbox.js
@@ -7,100 +7,99 @@
  */
 
 define(function(require) {
-	
-	var $ = require('jquery');
 
+	var $ = require('jquery');
 
 	// PILLBOX CONSTRUCTOR AND PROTOTYPE
 
-	var Pillbox = function (element, options) {
+	var Pillbox = function(element, options) {
 		this.$element = $(element);
 		this.options = $.extend({}, $.fn.pillbox.defaults, options);
 		this.$element.on('click', 'li', $.proxy(this.itemclicked, this));
 	};
 
 	Pillbox.prototype = {
-		constructor: Pillbox,
+		constructor : Pillbox,
 
-		items: function() {
+		items : function() {
 			return this.$element.find('li').map(function() {
 				var $this = $(this);
-				return $.extend({ text: $this.text() }, $this.data());
+				return $.extend({
+					text : $this.text()
+				}, $this.data());
 			}).get();
 		},
 
-		itemclicked: function (e) {
-		    
-		    var $li = $(e.currentTarget);
-		    var data = $.extend({ text: $li.html() }, $li.data());
-		  
+		itemclicked : function(e) {
+
+			var $li = $(e.currentTarget);
+			var data = $.extend({
+				text : $li.html()
+			}, $li.data());
+
 			$li.remove();
 			e.preventDefault();
-			
+
 			this.$element.trigger('remove', data);
 		},
-		
-		getItemCount: function () {
-		    
-		    return this.$element.find('li').length;
+
+		getItemCount : function() {
+
+			return this.$element.find('li').length;
 		},
-		
-		addItem: function (text, value) {
-		    
-		    value = value || text;
-		    
-		    //<li data-value="foo">Item One</li>
-		    
-		    var $li = $('<li />');
-		    
-		    $li
-		      .prop('data-value', value)
-		      .html(text)
-		    ;
-		    
-		    this.$element.find('ul').append($li);
-		    
-		    return $li;
+
+		addItem : function(text, value) {
+
+			value = value || text;
+
+			//<li data-value="foo">Item One</li>
+
+			var $li = $('<li data-value="' + value + '">' + text + '</li>');
+
+			this.$element.find('ul').append($li);
+
+			return $li;
 		},
-		
-		removeItemsBySelector: function (selector) {
-            
-           this.$element.find('ul').find(selector).remove();   
-        },
-		
-		removeItemsByValue: function (value) {
-		    
-		    var selector = 'li[data-value=' + value + ']';
-		    
-		    this.removeItemsBySelector(selector);       
+
+		removeItemsBySelector : function(selector) {
+
+			this.$element.find('ul').find(selector).remove();
 		},
-		
-		removeItemsByText: function (text) {
-		    
-		    var selector = 'li:contains("' + value + '")';
-		    
-		    this.removeItemsBySelector(selector);
+
+		removeItemsByValue : function(value) {
+
+			var selector = 'li[data-value=' + value + ']';
+
+			this.removeItemsBySelector(selector);
 		},
-		
-		removeItems: function () {
-		    
-		    this.$element.find('ul').empty();    
+
+		removeItemsByText : function(text) {
+
+			var selector = 'li:contains("' + text + '")';
+
+			this.removeItemsBySelector(selector);
+		},
+
+		removeItems : function() {
+
+			this.$element.find('ul').empty();
 		}
 	};
 
-
 	// PILLBOX PLUGIN DEFINITION
 
-	$.fn.pillbox = function (option) {
+	$.fn.pillbox = function(option, value1, value2) {
 		var methodReturn;
 
-		var $set = this.each(function () {
+		var $set = this.each(function() {
 			var $this = $(this);
 			var data = $this.data('pillbox');
 			var options = typeof option === 'object' && option;
 
-			if (!data) $this.data('pillbox', (data = new Pillbox(this, options)));
-			if (typeof option === 'string') methodReturn = data[option]();
+			if (!data)
+				$this.data('pillbox', ( data = new Pillbox(this, options)));
+			if ( typeof option === 'string')
+				methodReturn = data[option](value1, value2);
 		});
 
 		return (methodReturn === undefined) ? $set : methodReturn;
@@ -110,16 +109,16 @@ define(function(require) {
 
 	$.fn.pillbox.Constructor = Pillbox;
 
-
 	// PILLBOX DATA-API
 
-	$(function () {
-		$('body').on('mousedown.pillbox.data-api', '.pillbox', function () {
+	$(function() {
+		$('body').on('mousedown.pillbox.data-api', '.pillbox', function() {
 			var $this = $(this);
-			if ($this.data('pillbox')) return;
+			if ($this.data('pillbox'))
+				return;
 			$this.pillbox($this.data());
 		});
 	});
-	
+
 });
 
--- test/pillbox-test.js
+++ test/pillbox-test.js
@@ -30,5 +30,69 @@ require(['jquery', 'fuelux/pillbox'], function($) {
 		equal($pillbox.pillbox('items').length, 1, 'pillbox removed an item');
 		deepEqual($pillbox.pillbox('items')[0], {text: 'one', value: 'foo'}, 'pillbox returns item data');
 	});
-
+	
+	test("getItemCount function", function(){
+		var $pillbox = $(this.emptyPillboxHTML).pillbox();
+		
+		equal($pillbox.pillbox('getItemCount'), 0, 'getItemCount on empty pillbox');
+		
+		$pillbox = $(this.pillboxHTML).pillbox();
+		
+		equal($pillbox.pillbox('getItemCount'), 2, 'getItemCount on pillbox with 2 items');	
+	});
+	
+	test("addItem function", function(){
+		var $pillbox = $(this.emptyPillboxHTML).pillbox();
+		
+		equal($pillbox.pillbox('getItemCount'), 0, 'pillbox is initially empty');
+		
+		$pillbox.pillbox('addItem', 'Item 1', 1);
+		
+		equal($pillbox.pillbox('getItemCount'), 1, 'pillbox has 1 item after addItem called');		
+		deepEqual($pillbox.pillbox('items')[0], {text: 'Item 1', value: 1}, 'item added has correct text and value');
+	});
+	
+	test("addItem function with return value manipulation", function(){
+		var $pillbox = $(this.emptyPillboxHTML).pillbox();
+		
+		equal($pillbox.pillbox('getItemCount'), 0, 'pillbox is initially empty');
+		
+		var $item = $pillbox.pillbox('addItem', 'Item 1', 1);
+		$item.data('emailid', 123)
+		
+		equal($pillbox.pillbox('getItemCount'), 1, 'pillbox has 1 item after addItem called');		
+		deepEqual($pillbox.pillbox('items')[0], {text: 'Item 1', value: 1, emailid: 123}, 'item added has correct text, value, and emailid');
+	});
+	
+	test("removeItemsByValue function", function(){
+		var $pillbox = $(this.pillboxHTML).pillbox();
+		
+		equal($pillbox.pillbox('getItemCount'), 2, 'pillbox has 2 items initially');
+		
+		$pillbox.pillbox('removeItemsByValue', 'foo');
+		
+		equal($pillbox.pillbox('getItemCount'), 1, 'pillbox has 1 item after removeItemsByValue');		
+		deepEqual($pillbox.pillbox('items')[0], {text: 'two'}, 'item not removed has correct text and value');
+	});
+	
+	test("removeItemsByText function", function(){
+		var $pillbox = $(this.pillboxHTML).pillbox();
+		
+		equal($pillbox.pillbox('getItemCount'), 2, 'pillbox has 2 items initially');
+		
+		$pillbox.pillbox('removeItemsByText', 'two');
+		
+		equal($pillbox.pillbox('getItemCount'), 1, 'pillbox has 1 item after removeItemsByText');		
+		deepEqual($pillbox.pillbox('items')[0], {text: 'one', value: 'foo'}, 'item not removed has correct text and value');
+	});
+	
+	test("removeItems function", function(){
+		var $pillbox = $(this.pillboxHTML).pillbox();
+		
+		equal($pillbox.pillbox('getItemCount'), 2, 'pillbox has 2 items initially');
+		
+		$pillbox.pillbox('removeItems');
+		
+		equal($pillbox.pillbox('getItemCount'), 0, 'pillbox empty after removeItems');
+	});
 });
