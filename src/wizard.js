47d5e3168f97d991c7412b95c0be989043f904bf
Eli Gassert <eli.gassert@toad-software.com>
2013-09-06 12:56:46 -0400

    Accidentally removed the require wrapper.

--- src/wizard.js
+++ src/wizard.js
@@ -6,166 +6,183 @@
  * Licensed under the MIT license.
  */
 
+define(function (require) {
+
+	var $ = require('jquery');
+
 
 	// WIZARD CONSTRUCTOR AND PROTOTYPE
 
-	var Wizard = function (element, options) {
-		var kids;
-        var self = this;
+	var Wizard = function(element, options)
+	{
+	    var kids;
+	    var self = this;
 
-		this.$element = $(element);
-		this.options = $.extend({}, $.fn.wizard.defaults, options);
-		this.currentStep = 1;
-		this.numSteps = this.$element.find('.steps:first li').length;
-		this.$prevBtn = this.$element.find('button.btn-prev');
-		this.$nextBtn = this.$element.find('button.btn-next');
+	    this.$element = $(element);
+	    this.options = $.extend({}, $.fn.wizard.defaults, options);
+	    this.currentStep = 1;
+	    this.numSteps = this.$element.find('.steps:first li').length;
+	    this.$prevBtn = this.$element.find('button.btn-prev');
+	    this.$nextBtn = this.$element.find('button.btn-next');
 
-		this.$nextBtn.each(function()
-        {
-            kids = $(this).children().detach();
-		    self.nextText = $.trim($(this).text());
-            $(this).append(kids);
-        });
+	    this.$nextBtn.each(function()
+	    {
+	        kids = $(this).children().detach();
+	        self.nextText = $.trim($(this).text());
+	        $(this).append(kids);
+	    });
 
-		// handle events
-		this.$prevBtn.on('click', $.proxy(this.previous, this));
-		this.$nextBtn.on('click', $.proxy(this.next, this));
-		this.$element.on('click', 'li.complete', $.proxy(this.stepclicked, this));
+	    // handle events
+	    this.$prevBtn.on('click', $.proxy(this.previous, this));
+	    this.$nextBtn.on('click', $.proxy(this.next, this));
+	    this.$element.on('click', 'li.complete', $.proxy(this.stepclicked, this));
 	};
 
 	Wizard.prototype = {
 
-		constructor: Wizard,
+	    constructor: Wizard,
 
-		setState: function () {
-            var self = this;
+	    setState: function()
+	    {
+	        var self = this;
 
-			var canMovePrev = (this.currentStep > 1);
-			var firstStep = (this.currentStep === 1);
-			var lastStep = (this.currentStep === this.numSteps);
+	        var canMovePrev = (this.currentStep > 1);
+	        var firstStep = (this.currentStep === 1);
+	        var lastStep = (this.currentStep === this.numSteps);
 
-			// disable buttons based on current step
-			this.$prevBtn.each(function() { $(this).attr('disabled', (firstStep === true || canMovePrev === false)) });
+	        // disable buttons based on current step
+	        this.$prevBtn.each(function() { $(this).attr('disabled', (firstStep === true || canMovePrev === false)) });
 
-			// change button text of last step, if specified
-            this.$nextBtn.each(function()
-            {
-			    var data = $(this).data();
+	        // change button text of last step, if specified
+	        this.$nextBtn.each(function()
+	        {
+	            var data = $(this).data();
 
-			    if (data && data.last) {
-				    self.lastText = data.last;
-				    if (typeof self.lastText !== 'undefined') {
-					    // replace text
-					    var text = (lastStep !== true) ? self.nextText : self.lastText;
-					    var kids = $(this).children().detach();
-					    $(this).text(text).append(kids);
-				    }
-			    }
-            });
+	            if(data && data.last)
+	            {
+	                self.lastText = data.last;
+	                if(typeof self.lastText !== 'undefined')
+	                {
+	                    // replace text
+	                    var text = (lastStep !== true) ? self.nextText : self.lastText;
+	                    var kids = $(this).children().detach();
+	                    $(this).text(text).append(kids);
+	                }
+	            }
+	        });
 
-			// reset classes for all steps
-			var $steps = this.$element.find('.steps li');
-			$steps.removeClass('active').removeClass('complete');
-			$steps.find('span.badge').removeClass('badge-info').removeClass('badge-success');
+	        // reset classes for all steps
+	        var $steps = this.$element.find('.steps li');
+	        $steps.removeClass('active').removeClass('complete');
+	        $steps.find('span.badge').removeClass('badge-info').removeClass('badge-success');
 
-			
-            var targetChanged = false;
 
-            this.$element.find('.steps').each(function()
-            {
-                // set class for all previous steps
-                var prevSelector = 'li:lt(' + (self.currentStep - 1) + ')';
-			    var $prevSteps = $(this).find(prevSelector);
-			    $prevSteps.addClass('complete');
-			    $prevSteps.find('span.badge').addClass('badge-success');
+	        var targetChanged = false;
 
-			    // set class for current step
-			    var currentSelector = 'li:eq(' + (self.currentStep - 1) + ')';
-			    var $currentStep = $(this).find(currentSelector);
-			    $currentStep.addClass('active');
-			    $currentStep.find('span.badge').addClass('badge-info');
+	        this.$element.find('.steps').each(function()
+	        {
+	            // set class for all previous steps
+	            var prevSelector = 'li:lt(' + (self.currentStep - 1) + ')';
+	            var $prevSteps = $(this).find(prevSelector);
+	            $prevSteps.addClass('complete');
+	            $prevSteps.find('span.badge').addClass('badge-success');
 
-                // only process on the first steps target to avoid flicker
-                if(!targetChanged)
-                {
-                    targetChanged = true;
+	            // set class for current step
+	            var currentSelector = 'li:eq(' + (self.currentStep - 1) + ')';
+	            var $currentStep = $(this).find(currentSelector);
+	            $currentStep.addClass('active');
+	            $currentStep.find('span.badge').addClass('badge-info');
 
-			        // set display of target element
-			        var target = $currentStep.data().target;
-			        self.$element.find('.step-pane').removeClass('active');
-			        $(target).addClass('active');
-                }
-            });
+	            // only process on the first steps target to avoid flicker
+	            if(!targetChanged)
+	            {
+	                targetChanged = true;
 
-			this.$element.trigger('changed');
-		},
+	                // set display of target element
+	                var target = $currentStep.data().target;
+	                self.$element.find('.step-pane').removeClass('active');
+	                $(target).addClass('active');
+	            }
+	        });
 
-		stepclicked: function (e) {
-			var li = $(e.currentTarget);
+	        this.$element.trigger('changed');
+	    },
 
-			var index = $(e.currentTarget).closest('.steps').find('li').index(li);
+	    stepclicked: function(e)
+	    {
+	        var li = $(e.currentTarget);
 
-			var evt = $.Event('stepclick');
-			this.$element.trigger(evt, {step: index + 1});
-			if (evt.isDefaultPrevented()) return;
+	        var index = $(e.currentTarget).closest('.steps').find('li').index(li);
 
-			this.currentStep = (index + 1);
-			this.setState();
-		},
+	        var evt = $.Event('stepclick');
+	        this.$element.trigger(evt, { step: index + 1 });
+	        if(evt.isDefaultPrevented()) return;
 
-		previous: function () {
-			var canMovePrev = (this.currentStep > 1);
-			if (canMovePrev) {
-				var e = $.Event('change');
-				this.$element.trigger(e, {step: this.currentStep, direction: 'previous'});
-				if (e.isDefaultPrevented()) return;
+	        this.currentStep = (index + 1);
+	        this.setState();
+	    },
 
-				this.currentStep -= 1;
-				this.setState();
-			}
-		},
+	    previous: function()
+	    {
+	        var canMovePrev = (this.currentStep > 1);
+	        if(canMovePrev)
+	        {
+	            var e = $.Event('change');
+	            this.$element.trigger(e, { step: this.currentStep, direction: 'previous' });
+	            if(e.isDefaultPrevented()) return;
 
-		next: function () {
-			var canMoveNext = (this.currentStep + 1 <= this.numSteps);
-			var lastStep = (this.currentStep === this.numSteps);
+	            this.currentStep -= 1;
+	            this.setState();
+	        }
+	    },
 
-			if (canMoveNext) {
-				var e = $.Event('change');
-				this.$element.trigger(e, {step: this.currentStep, direction: 'next'});
+	    next: function()
+	    {
+	        var canMoveNext = (this.currentStep + 1 <= this.numSteps);
+	        var lastStep = (this.currentStep === this.numSteps);
 
-				if (e.isDefaultPrevented()) return;
+	        if(canMoveNext)
+	        {
+	            var e = $.Event('change');
+	            this.$element.trigger(e, { step: this.currentStep, direction: 'next' });
 
-				this.currentStep += 1;
-				this.setState();
-			}
-			else if (lastStep) {
-				this.$element.trigger('finished');
-			}
-		},
+	            if(e.isDefaultPrevented()) return;
 
-		selectedItem: function (val) {
-			return {
-				step: this.currentStep
-			};
-		}
+	            this.currentStep += 1;
+	            this.setState();
+	        }
+	        else if(lastStep)
+	        {
+	            this.$element.trigger('finished');
+	        }
+	    },
+
+	    selectedItem: function(val)
+	    {
+	        return {
+	            step: this.currentStep
+	        };
+	    }
 	};
 
 
 	// WIZARD PLUGIN DEFINITION
 
-	$.fn.wizard = function (option, value) {
-		var methodReturn;
+	$.fn.wizard = function(option, value)
+	{
+	    var methodReturn;
 
-		var $set = this.each(function () {
-			var $this = $(this);
-			var data = $this.data('wizard');
-			var options = typeof option === 'object' && option;
+	    var $set = this.each(function()
+	    {
+	        var $this = $(this);
+	        var data = $this.data('wizard');
+	        var options = typeof option === 'object' && option;
 
-			if (!data) $this.data('wizard', (data = new Wizard(this, options)));
-			if (typeof option === 'string') methodReturn = data[option](value);
-		});
+	        if(!data) $this.data('wizard', (data = new Wizard(this, options)));
+	        if(typeof option === 'string') methodReturn = data[option](value);
+	    });
 
-		return (methodReturn === undefined) ? $set : methodReturn;
+	    return (methodReturn === undefined) ? $set : methodReturn;
 	};
 
 	$.fn.wizard.defaults = {};
@@ -175,10 +192,14 @@
 
 	// WIZARD DATA-API
 
-	$(function () {
-		$('body').on('mousedown.wizard.data-api', '.wizard', function () {
-			var $this = $(this);
-			if ($this.data('wizard')) return;
-			$this.wizard($this.data());
-		});
+	$(function()
+	{
+	    $('body').on('mousedown.wizard.data-api', '.wizard', function()
+	    {
+	        var $this = $(this);
+	        if($this.data('wizard')) return;
+	        $this.wizard($this.data());
+	    });
 	});
+
+});
