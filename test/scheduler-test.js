91ce405132c936ecf241cc2ae31078e2fa410461
kevinparkerson <kparkerson@exacttarget.com>
2013-10-31 17:57:33 -0400

    prepping scheduler unit tests - gotta write a few

---
+++ test/scheduler-test.js
@@ -0,0 +1,14 @@
+/*global QUnit:false, module:false, test:false, asyncTest:false, expect:false*/
+/*global start:false, stop:false ok:false, equal:false, notEqual:false, deepEqual:false*/
+/*global notDeepEqual:false, strictEqual:false, notStrictEqual:false, raises:false*/
+
+require(['jquery', 'fuelux/scheduler'], function ($) {
+
+	var html = '';
+
+	module("Fuel UX scheduler");
+
+	test("should be bleh", function () {
+		ok(1===1, 'yeeeeee');
+	});
+});
