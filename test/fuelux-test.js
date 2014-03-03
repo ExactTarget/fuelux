7ec2938786c12e709fdfd674c196935e3719e784
kevinparkerson <kparkerson@exacttarget.com>
2014-03-03 13:30:19 -0500

    updating tests

--- test/fuelux-test.js
+++ test/fuelux-test.js
@@ -3,19 +3,22 @@
 /*global notDeepEqual:false, strictEqual:false, notStrictEqual:false, raises:false*/
 
 define(function(require){
+	var $ = require('jquery');
 
-	require('checkbox-test');
-	require('combobox-test');
-	require('datagrid-test');
-	require('datepicker-test');
-	require('datepicker-moment-test');
-	require('pillbox-test');
-	require('radio-test');
-	require('scheduler-test');
-	require('search-test');
-	require('spinner-test');
-	require('select-test');
-	require('tree-test');
-	require('wizard-test');
+	//TODO: make sure test files have appropriate dependencies
+
+	require('test/checkbox-test');
+	require('test/combobox-test');
+	require('test/datagrid-test');
+	require('test/datepicker-test');
+	require('test/datepicker-moment-test');
+	require('test/pillbox-test');
+	require('test/radio-test');
+	require('test/scheduler-test');
+	require('test/search-test');
+	require('test/spinner-test');
+	require('test/select-test');
+	require('test/tree-test');
+	require('test/wizard-test');
 
 });
