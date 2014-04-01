b6013baa76a308bceff338995182e5f096128a50
kevinparkerson <kparkerson@exacttarget.com>
2014-04-01 14:38:50 -0400

    moving the repeater-thumbnail styles to their own less file

--- js/repeater-thumbnail.js
+++ js/repeater-thumbnail.js
@@ -28,12 +28,12 @@
 		$.fn.repeater.views.thumbnail = {
 			renderer: {
 				render: function(helpers){
-					helpers.callback({ item: '<div class="clearfix thumbnailCont" data-container="true"></div>' });
+					helpers.callback({ item: '<div class="clearfix repeater-thumbnail-cont" data-container="true"></div>' });
 				},
 				nested: [
 					{
 						render: function(helpers){
-							helpers.callback({ item: '<div class="thumbnail" data-container="true" style="background: ' + helpers.subset[helpers.index].color + ';"><img height="75" src="' + helpers.subset[helpers.index].src + '" width="65">' + helpers.subset[helpers.index].name + '</div>' });
+							helpers.callback({ item: '<div class="thumbnail repeater-thumbnail" data-container="true" style="background: ' + helpers.subset[helpers.index].color + ';"><img height="75" src="' + helpers.subset[helpers.index].src + '" width="65">' + helpers.subset[helpers.index].name + '</div>' });
 						},
 						repeat: 'items'
 					}
