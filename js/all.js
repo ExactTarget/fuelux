54c4b5f4a44abdec578361af722cecbfcb82701d
kevinparkerson <kparkerson@exacttarget.com>
2014-04-07 10:30:43 -0400

    removing infinite scroll control (im going to put it on another branch)

--- js/all.js
+++ js/all.js
@@ -27,7 +27,6 @@
 	require('fuelux/combobox');
 	require('fuelux/datagrid');
 	require('fuelux/datepicker');
-	require('fuelux/infinite-scroll');
 	require('fuelux/intelligent-dropdown');
 	require('fuelux/loader');
 	require('fuelux/pillbox');
