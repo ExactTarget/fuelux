c8f97067eed287df94a8248dbe25ddb5ac7f2b43
kevinparkerson <kparkerson@exacttarget.com>
2014-06-02 16:34:31 -0400

    spinbox unit test updates

--- test/spinbox-test.js
+++ test/spinbox-test.js
@@ -15,11 +15,13 @@ define(function(require){
 	module("Fuel UX Spinbox");
 
 	test("should be defined on jquery object", function () {
-		ok($(document.body).spinbox, 'spinbox method is defined');
+		var $spinbox = $(html).find('#MySpinbox').spinbox();
+		ok($spinbox.spinbox, 'spinbox method is defined');
 	});
 
 	test("should return element", function () {
-		ok($(document.body).spinbox()[0] === document.body, 'document.body returned');
+		var $spinbox = $(html).find('#MySpinbox').spinbox();
+		ok($spinbox.spinbox() === $spinbox, 'spinbox should be initialized');
 	});
 
 	test("should behave as designed", function () {
