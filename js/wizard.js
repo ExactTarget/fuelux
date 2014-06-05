e638ed6a6613f6c6c798f3eca25b45e3d26d906e
Stephen James <llama@interactivellama.com>
2014-06-04 22:43:13 -0400

    Update wizard data api

--- js/wizard.js
+++ js/wizard.js
@@ -358,11 +358,11 @@
 
 	// DATA-API
 
-	// $('document').on('mouseover.fu.wizard.data-api', '[data-fuelux=wizard]', function () {
-	// 	var $this = $(this);
-	// 	if ($this.data('wizard')) { return; }
-	// 	$this.wizard($this.data());
-	// });
+	$('document').on('mouseover.fu.wizard.data-api', '[data-fuelux=wizard]', function () {
+		var $this = $(this);
+		if ($this.data('wizard')) { return; }
+		$this.wizard($this.data());
+	});
 
 	// Must be domReady for AMD compatibility
 	$(function () {
