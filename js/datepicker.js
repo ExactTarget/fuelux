bb077402df54fd23b80270a132711129dd3ef1aa
kevinparkerson <kparkerson@exacttarget.com>
2014-07-25 00:09:56 -0400

    datepicker renders dates

--- js/datepicker.js
+++ js/datepicker.js
@@ -41,8 +41,8 @@
 				if( typeof window.console !== 'undefined' ) {
 					if( window.navigator.userAgent.search( 'PhantomJS' ) < 0 ) {
 						// don't show this in phantomjs tests
-						window.console.log( "Don't worry if you're seeing a 404 that's looking for moment.js. The Fuel UX Datepicker is trying to use moment.js to give you extra features." );
-						window.console.log( "Checkout the Fuel UX docs (http://exacttarget.github.io/fuelux/#datepicker) to see how to integrate moment.js for more features" );
+						//window.console.log( "Don't worry if you're seeing a 404 that's looking for moment.js. The Fuel UX Datepicker is trying to use moment.js to give you extra features." );
+						//window.console.log( "Checkout the Fuel UX docs (http://exacttarget.github.io/fuelux/#datepicker) to see how to integrate moment.js for more features" );
 					}
 				}
 			}
@@ -55,11 +55,62 @@
 		this.$element = $(element);
 
 		this.options = $.extend(true, {}, $.fn.datepicker.defaults, options);
+
+		this.$days = this.$element.find('.datepicker-calendar-days');
+		this.$headerCenter = this.$element.find('.datepicker-calendar-header .center');
+
+		this.renderMonth(this.options.date);
 	};
 
 	Datepicker.prototype = {
 
-		constructor: Datepicker
+		constructor: Datepicker,
+
+		renderMonth: function(date){
+			var firstDay = new Date(date.getFullYear(), date.getMonth(), 1).getDay();
+			var lastDate = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+			var lastMonthDate = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
+			var $month = this.$headerCenter.find('.month');
+			var month = date.getMonth();
+			var stage = 0;
+			var $tbody = this.$days.find('tbody');
+			var year = date.getFullYear();
+			var curDate, i, j, $td, $tr;
+
+			$month.find('.current').removeClass('current');
+			$month.find('li[data-month="' + month + '"]').addClass('current');
+			this.$headerCenter.find('.year').text(year);
+			this.$headerCenter.find('a').attr({
+				'data-month': month,
+				'data-year': year
+			});
+
+			$tbody.empty();
+			curDate = lastMonthDate - firstDay + 1;
+			for(i=0; i<5; i++){
+				$tr = $('<tr></tr>');
+				for(j=0; j<7; j++){
+					$td = $('<td><span><a href="#">' + curDate + '</a></span></td>');
+					if(stage===0){
+						$td.addClass('last-month');
+					}else if(stage===2){
+						$td.addClass('next-month');
+					}
+
+					curDate++;
+					if(stage===0 && curDate>lastMonthDate){
+						curDate = 1;
+						stage = 1;
+					}else if(stage===1 && curDate>lastDate){
+						curDate = 1;
+						stage = 2;
+					}
+
+					$tr.append($td);
+				}
+				$tbody.append($tr);
+			}
+		}
 
 	};
 
@@ -82,7 +133,9 @@
 		return ( methodReturn === undefined ) ? $set : methodReturn;
 	};
 
-	$.fn.datepicker.defaults = {};
+	$.fn.datepicker.defaults = {
+		date: new Date()
+	};
 
 	$.fn.datepicker.Constructor = Datepicker;
 
