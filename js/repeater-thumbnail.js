42ce073e9ed23e93770770d18e732ee1c4a575c3
Christopher McCulloh <cmcculloh@exacttarget.com>
2015-02-09 14:03:30 -0500

    Revert "Repeater plugin api refactor"

--- js/repeater-thumbnail.js
+++ js/repeater-thumbnail.js
@@ -44,24 +44,24 @@
 			var i, $item, l, n;
 
 			//this function is necessary because lint yells when a function is in a loop
-			function compareItemIndex () {
+			var compareItemIndex = function () {
 				if (n === items[i].index) {
 					$item = $(this);
 					return false;
 				} else {
 					n++;
 				}
-			}
+			};
 
 			//this function is necessary because lint yells when a function is in a loop
-			function compareItemSelector () {
+			var compareItemSelector = function () {
 				$item = $(this);
 				if ($item.is(items[i].selector)) {
 					selectItem($item, items[i].selected);
 				}
-			}
+			};
 
-			function selectItem ($itm, select) {
+			var selectItem = function ($itm, select) {
 				select = (select !== undefined) ? select : true;
 				if (select) {
 					if (!force && selectable !== 'multi') {
@@ -72,7 +72,7 @@
 				} else {
 					$itm.removeClass('selected');
 				}
-			}
+			};
 
 			if (!$.isArray(items)) {
 				items = [items];
@@ -98,6 +98,7 @@
 				} else if (items[i].selector) {
 					this.$canvas.find('.repeater-thumbnail-cont .selectable').each(compareItemSelector);
 				}
+
 			}
 		};
 
@@ -112,118 +113,137 @@
 
 		//EXTENSION DEFINITION
 		$.fn.repeater.viewTypes.thumbnail = {
-			selected: function () {
+			selected: function (helpers, callback) {
 				var infScroll = this.viewOptions.thumbnail_infiniteScroll;
 				var opts;
 				if (infScroll) {
 					opts = (typeof infScroll === 'object') ? infScroll : {};
 					this.infiniteScrolling(true, opts);
 				}
-			},
-			before: function (helpers) {
-				var alignment = this.viewOptions.thumbnail_alignment;
-				var $cont = this.$canvas.find('.repeater-thumbnail-cont');
-				var response = {};
-				var $empty, validAlignments;
 
-				if ($cont.length < 1) {
-					$cont = $('<div class="clearfix repeater-thumbnail-cont" data-container="true" data-infinite="true" data-preserve="shallow"></div>');
-					if (alignment && alignment !== 'none') {
-						validAlignments = {
-							'center': 1,
-							'justify': 1,
-							'left': 1,
-							'right': 1
-						};
-						alignment = (validAlignments[alignment]) ? alignment : 'justify';
-						$cont.addClass('align-' + alignment);
-						this.thumbnail_injectSpacers = true;
-						response.item = $cont;
+				callback({});
+			},
+			renderer: {
+				render: function (helpers, callback) {
+					var alignment = this.viewOptions.thumbnail_alignment;
+					var $item = this.$canvas.find('.repeater-thumbnail-cont');
+					var obj = {};
+					var $empty, validAlignments;
+					if ($item.length > 0) {
+						obj.action = 'none';
 					} else {
-						this.thumbnail_injectSpacers = false;
-						response.action = 'none';
+						$item = $('<div class="clearfix repeater-thumbnail-cont" data-container="true" data-infinite="true" data-preserve="shallow"></div>');
+						if (alignment && alignment !== 'none') {
+							validAlignments = {
+								'center': 1,
+								'justify': 1,
+								'left': 1,
+								'right': 1
+							};
+							alignment = (validAlignments[alignment]) ? alignment : 'justify';
+							$item.addClass('align-' + alignment);
+							this.thumbnail_injectSpacers = true;
+						} else {
+							this.thumbnail_injectSpacers = false;
+						}
+
 					}
-				}
 
-				if (helpers.data.items.length < 1) {
-					$empty = $('<div class="empty"></div>');
-					$empty.append(this.viewOptions.thumbnail_noItemsHTML);
-					$cont.append($empty);
-				} else {
-					$cont.find('.empty:first').remove();
-				}
+					obj.item = $item;
+					if (helpers.data.items.length < 1) {
+						obj.skipNested = true;
+						$empty = $('<div class="empty"></div>');
+						$empty.append(this.viewOptions.thumbnail_noItemsHTML);
+						$item.append($empty);
+					} else {
+						$item.find('.empty:first').remove();
+					}
 
-				return response;
-			},
-			renderItem: function (helpers) {
-				var selectable = this.viewOptions.thumbnail_selectable;
-				var selected = 'selected';
-				var self = this;
-				var $thumbnail = $(fillTemplate(helpers.subset[helpers.index], this.viewOptions.thumbnail_template));
+					callback(obj);
+				},
+				nested: [
+					{
+						after: function (helpers, callback) {
+							var obj = {
+								container: helpers.container,
+								itemData: helpers.subset[helpers.index]
+							};
+							var selectable = this.viewOptions.thumbnail_selectable;
+							var selected = 'selected';
+							var self = this;
+							var $item;
+							if (helpers.item !== undefined) {
+								obj.item = helpers.item;
+								$item = $(obj.item);
+								if (selectable) {
+									$item.addClass('selectable');
+									$item.on('click', function () {
+										if (!$item.hasClass(selected)) {
+											if (selectable !== 'multi') {
+												self.$canvas.find('.repeater-thumbnail-cont .selectable.selected').each(function () {
+													var $itm = $(this);
+													$itm.removeClass(selected);
+													self.$element.trigger('deselected.fu.repeaterThumbnail', $itm);
+												});
+											}
 
-				if (selectable) {
-					$thumbnail.addClass('selectable');
-					$thumbnail.on('click', function () {
-						if (!$thumbnail.hasClass(selected)) {
-							if (selectable !== 'multi') {
-								self.$canvas.find('.repeater-thumbnail-cont .selectable.selected').each(function () {
-									var $itm = $(this);
-									$itm.removeClass(selected);
-									self.$element.trigger('deselected.fu.repeaterThumbnail', $itm);
-								});
+											$item.addClass(selected);
+											self.$element.trigger('selected.fu.repeaterThumbnail', $item);
+										} else {
+											$item.removeClass(selected);
+											self.$element.trigger('deselected.fu.repeaterThumbnail', $item);
+										}
+									});
+								}
+
+								if (this.thumbnail_injectSpacers) {
+									$item.after('<span class="spacer">&nbsp;</span>');
+								}
+
 							}
 
-							$thumbnail.addClass(selected);
-							self.$element.trigger('selected.fu.repeaterThumbnail', $thumbnail);
-						} else {
-							$thumbnail.removeClass(selected);
-							self.$element.trigger('deselected.fu.repeaterThumbnail', $thumbnail);
-						}
-					});
-				}
+							if (this.viewOptions.thumbnail_itemRendered) {
+								this.viewOptions.thumbnail_itemRendered(obj, function () {
+									callback();
+								});
+							} else {
+								callback();
+							}
+						},
+						render: function (helpers, callback) {
+							var item = helpers.subset[helpers.index];
+							var template = function (str) {
+								var invalid = false;
+								var replace = function () {
+									var end, start, val;
 
-				helpers.container.append($thumbnail);
-				if (this.thumbnail_injectSpacers) {
-					$thumbnail.after('<span class="spacer">&nbsp;</span>');
-				}
+									start = str.indexOf('{{');
+									end = str.indexOf('}}', start + 2);
 
-				if (this.viewOptions.thumbnail_itemRendered) {
-					this.viewOptions.thumbnail_itemRendered({
-						container: helpers.container,
-						item: $thumbnail,
-						itemData: helpers.subset[helpers.index]
-					}, function () {});
-				}
+									if (start > -1 && end > -1) {
+										val = $.trim(str.substring(start + 2, end));
+										val = (item[val] !== undefined) ? item[val] : '';
+										str = str.substring(0, start) + val + str.substring(end + 2);
+									} else {
+										invalid = true;
+									}
+								};
 
-				return false;
+								while (!invalid && str.search('{{') >= 0) {
+									replace(str);
+								}
+								return str;
+							};
+							callback({
+								item: template(this.viewOptions.thumbnail_template)
+							});
+						},
+						repeat: 'data.items'
+					}
+				]
 			}
 		};
-	}
 
-	//ADDITIONAL METHODS
-	function fillTemplate (itemData, template) {
-		var invalid = false;
-
-		function replace () {
-			var end, start, val;
-
-			start = template.indexOf('{{');
-			end = template.indexOf('}}', start + 2);
-
-			if (start > -1 && end > -1) {
-				val = $.trim(template.substring(start + 2, end));
-				val = (itemData[val] !== undefined) ? itemData[val] : '';
-				template = template.substring(0, start) + val + template.substring(end + 2);
-			} else {
-				invalid = true;
-			}
-		}
-
-		while (!invalid && template.search('{{') >= 0) {
-			replace(template);
-		}
-
-		return template;
 	}
 
 	// -- BEGIN UMD WRAPPER AFTERWORD --
