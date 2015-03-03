7a7b92adf2456dee7294f590f15b1b284321bf74
Christopher McCulloh <cmcculloh@gmail.com>
2015-03-03 14:30:09 -0500

    bower update

--- assets/vendor/fuelux/dist/js/fuelux.js
+++ assets/vendor/fuelux/dist/js/fuelux.js
@@ -1,5 +1,5 @@
 /*!
- * Fuel UX v3.5.2
+ * Fuel UX v3.6.3
  * Copyright 2012-2015 ExactTarget
  * Licensed under the BSD-3-Clause license (https://github.com/ExactTarget/fuelux/blob/master/LICENSE)
  */
@@ -2990,7 +2990,7 @@
 
 		// TREE CONSTRUCTOR AND PROTOTYPE
 
-		var Tree = function( element, options ) {
+		var Tree = function Tree( element, options ) {
 			this.$element = $( element );
 			this.options = $.extend( {}, $.fn.tree.defaults, options );
 
@@ -3001,13 +3001,13 @@
 			}
 
 			this.$element.on( 'click.fu.tree', '.tree-branch-name', $.proxy( function( ev ) {
-				this.openFolder( ev.currentTarget );
+				this.toggleFolder( ev.currentTarget );
 			}, this ) );
 
 			if ( this.options.folderSelect ) {
 				this.$element.off( 'click.fu.tree', '.tree-branch-name' );
 				this.$element.on( 'click.fu.tree', '.icon-caret', $.proxy( function( ev ) {
-					this.openFolder( $( ev.currentTarget ).parent() );
+					this.toggleFolder( $( ev.currentTarget ).parent() );
 				}, this ) );
 				this.$element.on( 'click.fu.tree', '.tree-branch-name', $.proxy( function( ev ) {
 					this.selectFolder( $( ev.currentTarget ) );
@@ -3020,7 +3020,7 @@
 		Tree.prototype = {
 			constructor: Tree,
 
-			destroy: function() {
+			destroy: function destroy() {
 				// any external bindings [none]
 				// empty elements to return to original markup
 				this.$element.find( "li:not([data-template])" ).remove();
@@ -3030,11 +3030,11 @@
 				return this.$element[ 0 ].outerHTML;
 			},
 
-			render: function() {
+			render: function render() {
 				this.populate( this.$element );
 			},
 
-			populate: function( $el ) {
+			populate: function populate( $el ) {
 				var self = this;
 				var $parent = ( $el.hasClass( 'tree' ) ) ? $el : $el.parent();
 				var loader = $parent.find( '.tree-loader:eq(0)' );
@@ -3048,11 +3048,11 @@
 						var $entity;
 
 						if ( value.type === 'folder' ) {
-							$entity = self.$element.find( '[data-template=treebranch]:eq(0)' ).clone().removeClass( 'hide' ).removeAttr( 'data-template' );
+							$entity = self.$element.find( '[data-template=treebranch]:eq(0)' ).clone().removeClass( 'hide' ).removeData( 'template' );
 							$entity.data( value );
 							$entity.find( '.tree-branch-name > .tree-label' ).html( value.text || value.name );
 						} else if ( value.type === 'item' ) {
-							$entity = self.$element.find( '[data-template=treeitem]:eq(0)' ).clone().removeClass( 'hide' ).removeAttr( 'data-template' );
+							$entity = self.$element.find( '[data-template=treeitem]:eq(0)' ).clone().removeClass( 'hide' ).removeData( 'template' );
 							$entity.find( '.tree-item-name > .tree-label' ).html( value.text || value.name );
 							$entity.data( value );
 						}
@@ -3077,7 +3077,7 @@
 						// the "name" attribute is also supported but is deprecated for "text".
 
 						// add attributes to tree-branch or tree-item
-						var attr = value[ 'attr' ] || value.dataAttributes || [];
+						var attr = value.attr || value.dataAttributes || [];
 						$.each( attr, function( key, value ) {
 							switch ( key ) {
 								case 'cssClass':
@@ -3099,7 +3099,7 @@
 									$entity.find( '.tree-branch-name > .tree-label' ).attr( 'id', value + '-label' );
 									break;
 
-									// id, style, data-*
+									// style, data-*
 								default:
 									$entity.attr( key, value );
 									break;
@@ -3119,7 +3119,7 @@
 				} );
 			},
 
-			selectItem: function( el ) {
+			selectItem: function selectItem( el ) {
 				if ( !this.options.itemSelect ) return;
 				var $el = $( el );
 				var selData = $el.data();
@@ -3175,60 +3175,67 @@
 				} );
 			},
 
-			openFolder: function( el ) {
-				var $el = $( el ); // tree-branch-name
-				var $branch;
-				var $treeFolderContent;
-				var $treeFolderContentFirstChild;
+			openFolder: function openFolder( el, ignoreRedundantOpens ) {
+				var $el = $( el );
 
-				// if item select only
-				if ( !this.options.folderSelect ) {
-					$el = $( el ).parent(); // tree-branch, if tree-branch-name clicked
+				//don't break the API :| (make this functionally the same as calling 'toggleFolder')
+				if ( !ignoreRedundantOpens && $el.find( '.glyphicon-folder-open' ).length && !this.options.ignoreRedundantOpens ) {
+					this.closeFolder( el );
 				}
 
-				$branch = $el.closest( '.tree-branch' ); // tree branch
-				$treeFolderContent = $branch.find( '.tree-branch-children' );
-				$treeFolderContentFirstChild = $treeFolderContent.eq( 0 );
+				var $branch = $el.closest( '.tree-branch' );
+				var $treeFolderContent = $branch.find( '.tree-branch-children' );
+				var $treeFolderContentFirstChild = $treeFolderContent.eq( 0 );
 
-				// manipulate branch/folder
-				var eventType, classToTarget, classToAdd;
-				if ( $el.find( '.glyphicon-folder-close' ).length ) {
-					eventType = 'opened';
-					classToTarget = '.glyphicon-folder-close';
-					classToAdd = 'glyphicon-folder-open';
-
-					$branch.addClass( 'tree-open' );
-					$branch.attr( 'aria-expanded', 'true' );
-
-					$treeFolderContentFirstChild.removeClass( 'hide' );
-					if ( !$treeFolderContent.children().length ) {
-						this.populate( $treeFolderContent );
-					}
-
-				} else if ( $el.find( '.glyphicon-folder-open' ) ) {
-					eventType = 'closed';
-					classToTarget = '.glyphicon-folder-open';
-					classToAdd = 'glyphicon-folder-close';
-
-					$branch.removeClass( 'tree-open' );
-					$branch.attr( 'aria-expanded', 'false' );
-					$treeFolderContentFirstChild.addClass( 'hide' );
-
-					// remove if no cache
-					if ( !this.options.cacheItems ) {
-						$treeFolderContentFirstChild.empty();
-					}
-
-				}
-
+				//take care of the styles
+				$branch.addClass( 'tree-open' );
+				$branch.attr( 'aria-expanded', 'true' );
+				$treeFolderContentFirstChild.removeClass( 'hide' );
 				$branch.find( '> .tree-branch-header .icon-folder' ).eq( 0 )
-					.removeClass( 'glyphicon-folder-close glyphicon-folder-open' )
-					.addClass( classToAdd );
+					.removeClass( 'glyphicon-folder-close' )
+					.addClass( 'glyphicon-folder-open' );
 
-				this.$element.trigger( eventType + '.fu.tree', $branch.data() );
+				//add the children to the folder
+				if ( !$treeFolderContent.children().length ) {
+					this.populate( $treeFolderContent );
+				}
+
+				this.$element.trigger( 'opened.fu.tree', $branch.data() );
 			},
 
-			selectFolder: function( clickedElement ) {
+			closeFolder: function closeFolder( el ) {
+				var $el = $( el );
+				var $branch = $el.closest( '.tree-branch' );
+				var $treeFolderContent = $branch.find( '.tree-branch-children' );
+				var $treeFolderContentFirstChild = $treeFolderContent.eq( 0 );
+
+				//take care of the styles
+				$branch.removeClass( 'tree-open' );
+				$branch.attr( 'aria-expanded', 'false' );
+				$treeFolderContentFirstChild.addClass( 'hide' );
+				$branch.find( '> .tree-branch-header .icon-folder' ).eq( 0 )
+					.removeClass( 'glyphicon-folder-open' )
+					.addClass( 'glyphicon-folder-close' );
+
+				// remove chidren if no cache
+				if ( !this.options.cacheItems ) {
+					$treeFolderContentFirstChild.empty();
+				}
+
+				this.$element.trigger( 'closed.fu.tree', $branch.data() );
+			},
+
+			toggleFolder: function toggleFolder( el ) {
+				var $el = $( el );
+
+				if ( $el.find( '.glyphicon-folder-close' ).length ) {
+					this.openFolder( el );
+				} else if ( $el.find( '.glyphicon-folder-open' ).length ) {
+					this.closeFolder( el );
+				}
+			},
+
+			selectFolder: function selectFolder( clickedElement ) {
 				if ( !this.options.folderSelect ) return;
 				var $clickedElement = $( clickedElement );
 				var $clickedBranch = $clickedElement.closest( '.tree-branch' );
@@ -3276,7 +3283,7 @@
 				} );
 			},
 
-			selectedItems: function() {
+			selectedItems: function selectedItems() {
 				var $sel = this.$element.find( '.tree-selected' );
 				var data = [];
 
@@ -3287,32 +3294,146 @@
 			},
 
 			// collapses open folders
-			collapse: function() {
-				var cacheItems = this.options.cacheItems;
+			collapse: function collapse() {
+				var self = this;
+				var reportedClosed = [];
 
-				// find open folders
-				this.$element.find( '.icon-folder-open' ).each( function() {
-					// update icon class
-					var $this = $( this )
-						.removeClass( 'icon-folder-close icon-folder-open' )
-						.addClass( 'icon-folder-close' );
+				var closedReported = function closedReported( event, closed ) {
+					reportedClosed.push( closed );
 
-					// "close" or empty folder contents
-					var $parent = $this.parent().parent();
-					var $folder = $parent.children( '.tree-branch-children' );
-
-					$folder.addClass( 'hide' );
-					if ( !cacheItems ) {
-						$folder.empty();
+					if ( self.$element.find( ".tree-branch.tree-open:not('.hide')" ).length === 0 ) {
+						self.$element.trigger( 'closedAll.fu.tree', {
+							tree: self.$element,
+							reportedClosed: reportedClosed
+						} );
+						self.$element.off( 'loaded.fu.tree', self.$element, closedReported );
 					}
+				};
+
+				//trigger callback when all folders have reported closed
+				self.$element.on( 'closed.fu.tree', closedReported );
+
+				self.$element.find( ".tree-branch.tree-open:not('.hide')" ).each( function() {
+					self.closeFolder( this );
 				} );
+			},
+
+			//disclose visible will only disclose visible tree folders
+			discloseVisible: function discloseVisible() {
+				var self = this;
+				var $openableFolders = self.$element.find( ".tree-branch:not('.tree-open, .hide')" );
+				var reportedOpened = [];
+
+				var openReported = function openReported( event, opened ) {
+					reportedOpened.push( opened );
+
+					if ( reportedOpened.length === $openableFolders.length ) {
+						self.$element.trigger( 'disclosedVisible.fu.tree', {
+							tree: self.$element,
+							reportedOpened: reportedOpened
+						} );
+						/*
+						 * Unbind the `openReported` event. `discloseAll` may be running and we want to reset this
+						 * method for the next iteration.
+						 */
+						self.$element.off( 'loaded.fu.tree', self.$element, openReported );
+					}
+				};
+
+				//trigger callback when all folders have reported opened
+				self.$element.on( 'loaded.fu.tree', openReported );
+
+				// open all visible folders
+				self.$element.find( ".tree-branch:not('.tree-open, .hide')" ).each( function triggerOpen() {
+					self.openFolder( $( this ).find( '.tree-branch-header' ), true );
+				} );
+			},
+
+			/**
+			 * Disclose all will keep listening for `loaded.fu.tree` and if `$(tree-el).data('ignore-disclosures-limit')`
+			 * is `true` (defaults to `true`) it will attempt to disclose any new closed folders than were
+			 * loaded in during the last disclosure.
+			 */
+			discloseAll: function discloseAll() {
+				var self = this;
+
+				//first time
+				if ( typeof self.$element.data( 'disclosures' ) === 'undefined' ) {
+					self.$element.data( 'disclosures', 0 );
+				}
+
+				var isExceededLimit = ( self.options.disclosuresUpperLimit >= 1 && self.$element.data( 'disclosures' ) >= self.options.disclosuresUpperLimit );
+				var isAllDisclosed = self.$element.find( ".tree-branch:not('.tree-open, .hide')" ).length === 0;
+
+
+				if ( !isAllDisclosed ) {
+					if ( isExceededLimit ) {
+						self.$element.trigger( 'exceededDisclosuresLimit.fu.tree', {
+							tree: self.$element,
+							disclosures: self.$element.data( 'disclosures' )
+						} );
+
+						/*
+						 * If you've exceeded the limit, the loop will be killed unless you
+						 * explicitly ignore the limit and start the loop again:
+						 *
+						 *    $tree.one('exceededDisclosuresLimit.fu.tree', function () {
+						 *        $tree.data('ignore-disclosures-limit', true);
+						 *        $tree.tree('discloseAll');
+						 *    });
+						 */
+						if ( !self.$element.data( 'ignore-disclosures-limit' ) ) {
+							return;
+						}
+
+					}
+
+					self.$element.data( 'disclosures', self.$element.data( 'disclosures' ) + 1 );
+
+					/*
+					 * A new branch that is closed might be loaded in, make sure those get handled too.
+					 * This attachment needs to occur before calling `discloseVisible` to make sure that
+					 * if the execution of `discloseVisible` happens _super fast_ (as it does in our QUnit tests
+					 * this will still be called. However, make sure this only gets called _once_, because
+					 * otherwise, every single time we go through this loop, _another_ event will be bound
+					 * and then when the trigger happens, this will fire N times, where N equals the number
+					 * of recursive `discloseAll` executions (instead of just one)
+					 */
+					self.$element.one( 'disclosedVisible.fu.tree', function() {
+						self.discloseAll();
+					} );
+
+					/*
+					 * If the page is very fast, calling this first will cause `disclosedVisible.fu.tree` to not
+					 * be bound in time to be called, so, we need to call this last so that the things bound
+					 * and triggered above can have time to take place before the next execution of the
+					 * `discloseAll` method.
+					 */
+					self.discloseVisible();
+				} else {
+					self.$element.trigger( 'disclosedAll.fu.tree', {
+						tree: self.$element,
+						disclosures: self.$element.data( 'disclosures' )
+					} );
+
+					//if `cacheItems` is false, and they call closeAll, the data is trashed and therefore
+					//disclosures needs to accurately reflect that
+					if ( !self.options.cacheItems ) {
+						self.$element.one( 'closeAll.fu.tree', function() {
+							self.$element.data( 'disclosures', 0 );
+						} );
+					}
+
+				}
 			}
 		};
 
+		//alias for collapse for consistency. "Collapse" is an ambiguous term (collapse what? All? One specific branch?)
+		Tree.prototype.closeAll = Tree.prototype.collapse;
 
 		// TREE PLUGIN DEFINITION
 
-		$.fn.tree = function( option ) {
+		$.fn.tree = function tree( option ) {
 			var args = Array.prototype.slice.call( arguments, 1 );
 			var methodReturn;
 
@@ -3334,11 +3455,41 @@
 		};
 
 		$.fn.tree.defaults = {
-			dataSource: function( options, callback ) {},
+			dataSource: function dataSource( options, callback ) {},
 			multiSelect: false,
 			cacheItems: true,
 			folderSelect: true,
-			itemSelect: true
+			itemSelect: true,
+			/*
+			 * Calling "open" on something, should do that. However, the current API
+			 * instead treats "open" as a "toggle" and will close a folder that is open
+			 * if you call `openFolder` on it. Setting `ignoreRedundantOpens` to `true`
+			 * will make the folder instead ignore the redundant call and stay open.
+			 * This allows you to fix the API until 3.7.x when we can deprecate the switch
+			 * and make `openFolder` behave correctly by default.
+			 */
+			ignoreRedundantOpens: false,
+			/*
+			 * How many times `discloseAll` should be called before a stopping and firing
+			 * an `exceededDisclosuresLimit` event. You can force it to continue by
+			 * listening for this event, setting `ignore-disclosures-limit` to `true` and
+			 * starting `discloseAll` back up again. This lets you make more decisions
+			 * about if/when/how/why/how many times `discloseAll` will be started back
+			 * up after it exceeds the limit.
+			 *
+			 *    $tree.one('exceededDisclosuresLimit.fu.tree', function () {
+			 *        $tree.data('ignore-disclosures-limit', true);
+			 *        $tree.tree('discloseAll');
+			 *    });
+			 *
+			 * `disclusuresUpperLimit` defaults to `0`, so by default this trigger
+			 * will never fire. The true hard the upper limit is the browser's
+			 * ability to load new items (i.e. it will keep loading until the browser
+			 * falls over and dies). On the Fuel UX `index.html` page, the point at
+			 * which the page became super slow (enough to seem almost unresponsive)
+			 * was `4`, meaning 256 folders had been opened, and 1024 were attempting to open.
+			 */
+			disclosuresUpperLimit: 0
 		};
 
 		$.fn.tree.Constructor = Tree;
