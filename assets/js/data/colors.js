830741721a7e99a581842be26d69f1af84519d0d
Christopher McCulloh <cmcculloh@gmail.com>
2015-03-04 09:54:04 -0500

    cleans up repeater example to make it a bit more readable

---
+++ assets/js/data/colors.js
@@ -0,0 +1,22 @@
+define(function (require) {
+	return {
+		bug: '#A8B820',
+		dark: '#705848',
+		dragon: '#7038F8',
+		electric: '#F8D030',
+		fairy: '#EE99AC',
+		fighting: '#C03028',
+		fire: '#F08030',
+		flying: '#A890F0',
+		ghost: '#705898',
+		grass: '#78C850',
+		ground: '#E0C068',
+		ice: '#98D8D8',
+		normal: '#A8A878',
+		poison: '#A040A0',
+		psychic: '#F85888',
+		rock: '#B8A038',
+		steel: '#B8B8D0',
+		water: '#6890F0'
+	};
+});
