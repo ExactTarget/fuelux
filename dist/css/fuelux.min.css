8d44fbf1a564a7e23a9d0688e0a68af94d55604c
Christopher McCulloh <cmcculloh@exacttarget.com>
2015-03-17 16:20:31 -0400

    (3.x) 3.6.4 release

--- dist/css/fuelux.min.css
+++ dist/css/fuelux.min.css
@@ -1,6 +1,5 @@
 /*!
- * Fuel UX EDGE - Built 2015/03/17, 1:39:33 PM 
- * Previous release: v3.6.3 
+ * Fuel UX v3.6.4 
  * Copyright 2012-2015 ExactTarget
  * Licensed under the BSD-3-Clause license (https://github.com/ExactTarget/fuelux/blob/master/LICENSE)
  */
