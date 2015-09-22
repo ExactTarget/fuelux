a026474077b7d1964c0195a1c89d07120502df08
swilliams <swilliams@exacttarget.com>
2015-09-22 15:41:27 -0400

    add disabled attribute to repeater object and look for it on click of selectable list (fix #1534)

--- js/repeater-list.js
+++ js/repeater-list.js
@@ -594,6 +594,8 @@
 		if (sortable) {
 			$both.addClass('sortable');
 			$div.on('click.fu.repeaterList', function () {
+				if(self.isDisabled) return;
+
 				self.list_sortProperty = (typeof sortable === 'string') ? sortable : columns[index].property;
 				if ($div.hasClass('sorted')) {
 					if ($span.hasClass(chevUp)) {
@@ -655,6 +657,8 @@
 			$row.data('item_data', rows[index]);
 
 			$row.on('click.fu.repeaterList', function () {
+				if(self.isDisabled) return;
+
 				var $item = $(this);
 				var index = $(this).index();
 				index = index + 1;
