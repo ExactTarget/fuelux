f5ecab4c72b6e3440904f4eb7efa6e88013a9f37
Christopher McCulloh <cmcculloh@exacttarget.com>
2015-12-02 17:32:13 -0500

    WIP pended combobox and button dropdown

--- assets/js/form-builder/templates/component/component-templates.js
+++ assets/js/form-builder/templates/component/component-templates.js
@@ -1,7 +1,9 @@
 define(function ComponentTemplates(require) {
 	var formname = require('text!templates/component/formname.html');
 	var input = require('text!templates/component/input.html');
+	var pendedbuttondropdown = require('text!templates/component/pendedbuttondropdown.html');
 	var pendedcheckbox = require('text!templates/component/pendedcheckbox.html');
+	var pendedcombobox = require('text!templates/component/pendedcombobox.html');
 	var pendedsearch = require('text!templates/component/pendedsearch.html');
 	var pendedtext = require('text!templates/component/pendedtext.html');
 	var spinbox = require('text!templates/component/spinbox.html');
@@ -11,10 +13,6 @@ define(function ComponentTemplates(require) {
 	var filebutton = require('text!templates/component/filebutton.html');
 	var button = require('text!templates/component/button.html');
 	var buttondouble = require('text!templates/component/buttondouble.html');
-	var prependbuttondropdown = require('text!templates/component/prependbuttondropdown.html');
-	var appendbuttondropdown = require('text!templates/component/appendbuttondropdown.html');
-	var prependcombobox = require('text!templates/component/prependcombobox.html');
-	var appendcombobox = require('text!templates/component/appendcombobox.html');
 	var multiplecheckboxes = require('text!templates/component/multiplecheckboxes.html');
 	var multiplecheckboxesinline = require('text!templates/component/multiplecheckboxesinline.html');
 	var multipleradios = require('text!templates/component/multipleradios.html');
@@ -27,7 +25,9 @@ define(function ComponentTemplates(require) {
 		/* audited */
 		formname: formname,
 		input: input,
+		pendedbuttondropdown: pendedbuttondropdown,
 		pendedcheckbox: pendedcheckbox,
+		pendedcombobox: pendedcombobox,
 		pendedsearch: pendedsearch,
 		pendedtext: pendedtext,
 		spinbox: spinbox,
@@ -37,10 +37,6 @@ define(function ComponentTemplates(require) {
 		filebutton: filebutton,
 		singlebutton: button,
 		doublebutton: buttondouble,
-		prependbuttondropdown: prependbuttondropdown,
-		appendbuttondropdown: appendbuttondropdown,
-		prependcombobox: prependcombobox,
-		appendcombobox: appendcombobox,
 		multiplecheckboxes: multiplecheckboxes,
 		multiplecheckboxesinline: multiplecheckboxesinline,
 		multipleradios: multipleradios,
