f881e13557bfb0ac725c6a67f48b0e8f51c02059
Christopher McCulloh <cmcculloh@exacttarget.com>
2015-12-03 15:20:17 -0500

    audits fuelux checkboxes

--- assets/js/form-builder/templates/component/component-templates.js
+++ assets/js/form-builder/templates/component/component-templates.js
@@ -1,5 +1,7 @@
 define(function ComponentTemplates(require) {
 	var formname = require('text!templates/component/formname.html');
+	var fueluxcheckboxes = require('text!templates/component/fueluxcheckboxes.html');
+	var fueluxcheckboxesinline = require('text!templates/component/fueluxcheckboxesinline.html');
 	var input = require('text!templates/component/input.html');
 	var inputwithbuttondropdown = require('text!templates/component/inputwithbuttondropdown.html');
 	var inputwithcheckbox = require('text!templates/component/inputwithcheckbox.html');
@@ -9,12 +11,9 @@ define(function ComponentTemplates(require) {
 	var spinbox = require('text!templates/component/spinbox.html');
 	var textarea = require('text!templates/component/textarea.html');
 
-
 	var filebutton = require('text!templates/component/filebutton.html');
 	var button = require('text!templates/component/button.html');
 	var buttondouble = require('text!templates/component/buttondouble.html');
-	var multiplecheckboxes = require('text!templates/component/multiplecheckboxes.html');
-	var multiplecheckboxesinline = require('text!templates/component/multiplecheckboxesinline.html');
 	var multipleradios = require('text!templates/component/multipleradios.html');
 	var multipleradiosinline = require('text!templates/component/multipleradiosinline.html');
 	var selectbasic = require('text!templates/component/selectbasic.html');
@@ -24,6 +23,8 @@ define(function ComponentTemplates(require) {
 	return {
 		/* audited */
 		formname: formname,
+		fueluxcheckboxes: fueluxcheckboxes,
+		fueluxcheckboxesinline: fueluxcheckboxesinline,
 		input: input,
 		inputwithbuttondropdown: inputwithbuttondropdown,
 		inputwithcheckbox: inputwithcheckbox,
@@ -37,8 +38,6 @@ define(function ComponentTemplates(require) {
 		filebutton: filebutton,
 		singlebutton: button,
 		doublebutton: buttondouble,
-		multiplecheckboxes: multiplecheckboxes,
-		multiplecheckboxesinline: multiplecheckboxesinline,
 		multipleradios: multipleradios,
 		multipleradiosinline: multipleradiosinline,
 		selectbasic: selectbasic,
