49dc6adf31c6b831a00b28b5913d2e68dcea1bb2
Christopher McCulloh <cmcculloh@exacttarget.com>
2016-02-23 15:20:09 -0500

    (master) ran dist

--- dist/js/fuelux.js
+++ dist/js/fuelux.js
@@ -1,5 +1,5 @@
 /*!
- * Fuel UX EDGE - Built 2016/02/22, 5:40:55 PM 
+ * Fuel UX EDGE - Built 2016/02/23, 3:19:56 PM 
  * Previous release: v3.14.1 
  * Copyright 2012-2016 ExactTarget
  * Licensed under the BSD-3-Clause license (https://github.com/ExactTarget/fuelux/blob/master/LICENSE)
@@ -255,10 +255,12 @@
 			this.$dropMenu = this.$element.find( '.dropdown-menu' );
 			this.$input = this.$element.find( 'input' );
 			this.$button = this.$element.find( '.btn' );
+			this.$inputGroupBtn = this.$element.find( '.input-group-btn' );
 
 			this.$element.on( 'click.fu.combobox', 'a', $.proxy( this.itemclicked, this ) );
 			this.$element.on( 'change.fu.combobox', 'input', $.proxy( this.inputchanged, this ) );
 			this.$element.on( 'shown.bs.dropdown', $.proxy( this.menuShown, this ) );
+			this.$input.on( 'keyup.fu.combobox', $.proxy( this.keypress, this ) );
 
 			// set default selection
 			this.setDefaultSelection();
@@ -267,6 +269,11 @@
 			var items = this.$dropMenu.children( 'li' );
 			if ( items.length === 0 ) {
 				this.$button.addClass( 'disabled' );
+			}
+
+			// filter on load in case the first thing they do is press navigational key to pop open the menu
+			if ( this.options.filterOnKeypress ) {
+				this.options.filter( this.$dropMenu.find( 'li' ), this.$input.val(), this );
 			}
 
 		};
@@ -293,11 +300,18 @@
 
 			doSelect: function( $item ) {
 				if ( typeof $item[ 0 ] !== 'undefined' ) {
+					$item.addClass( 'selected' );
 					this.$selectedItem = $item;
 					this.$input.val( this.$selectedItem.text().trim() );
 				} else {
 					this.$selectedItem = null;
 				}
+			},
+
+			clearSelection: function() {
+				this.$selectedItem = null;
+				this.$input.val( '' );
+				this.$dropMenu.find( 'li' ).removeClass( 'selected' );
 			},
 
 			menuShown: function() {
@@ -322,7 +336,7 @@
 					}, this.$selectedItem.data() );
 				} else {
 					data = {
-						text: this.$input.val()
+						text: this.$input.val().trim()
 					};
 				}
 
@@ -332,11 +346,12 @@
 			selectByText: function( text ) {
 				var $item = $( [] );
 				this.$element.find( 'li' ).each( function() {
-					if ( ( this.textContent || this.innerText || $( this ).text() || '' ).toLowerCase() === ( text || '' ).toLowerCase() ) {
+					if ( ( this.textContent || this.innerText || $( this ).text() || '' ).trim().toLowerCase() === ( text || '' ).trim().toLowerCase() ) {
 						$item = $( this );
 						return false;
 					}
 				} );
+
 				this.doSelect( $item );
 			},
 
@@ -401,6 +416,75 @@
 				this.$element.find( '.dropdown-toggle' ).focus();
 			},
 
+			keypress: function( e ) {
+				var ENTER = 13;
+				//var TAB = 9;
+				var ESC = 27;
+				var LEFT = 37;
+				var UP = 38;
+				var RIGHT = 39;
+				var DOWN = 40;
+
+				var IS_NAVIGATIONAL = (
+					e.which === UP ||
+					e.which === DOWN ||
+					e.which === LEFT ||
+					e.which === RIGHT
+				);
+
+				if ( this.options.showOptionsOnKeypress && !this.$inputGroupBtn.hasClass( 'open' ) ) {
+					this.$button.dropdown( 'toggle' );
+					this.$input.focus();
+				}
+
+				if ( e.which === ENTER ) {
+					e.preventDefault();
+
+					var selected = this.$dropMenu.find( 'li.selected' ).text().trim();
+					if ( selected.length > 0 ) {
+						this.selectByText( selected );
+					} else {
+						this.selectByText( this.$input.val() );
+					}
+
+					this.$inputGroupBtn.removeClass( 'open' );
+					this.inputchanged( e );
+				} else if ( e.which === ESC ) {
+					e.preventDefault();
+					this.clearSelection();
+					this.$inputGroupBtn.removeClass( 'open' );
+				} else if ( this.options.showOptionsOnKeypress ) {
+					if ( e.which === DOWN || e.which === UP ) {
+						e.preventDefault();
+						var $selected = this.$dropMenu.find( 'li.selected' );
+						if ( $selected.length > 0 ) {
+							if ( e.which === DOWN ) {
+								$selected = $selected.next( ':not(.hidden)' );
+							} else {
+								$selected = $selected.prev( ':not(.hidden)' );
+							}
+						}
+
+						if ( $selected.length === 0 ) {
+							if ( e.which === DOWN ) {
+								$selected = this.$dropMenu.find( 'li:not(.hidden):first' );
+							} else {
+								$selected = this.$dropMenu.find( 'li:not(.hidden):last' );
+							}
+						}
+						this.$dropMenu.find( 'li' ).removeClass( 'selected' );
+						$selected.addClass( 'selected' );
+					}
+				}
+
+				// Avoid filtering on navigation key presses
+				if ( this.options.filterOnKeypress && !IS_NAVIGATIONAL ) {
+					this.options.filter( this.$dropMenu.find( 'li' ), this.$input.val(), this );
+				}
+
+				this.previousKeyPress = e.which;
+			},
+
 			inputchanged: function( e, extra ) {
 				// skip processing for internally-generated synthetic event
 				// to avoid double processing
@@ -448,7 +532,34 @@
 		};
 
 		$.fn.combobox.defaults = {
-			autoResizeMenu: true
+			autoResizeMenu: true,
+			filterOnKeypress: false,
+			showOptionsOnKeypress: false,
+			filter: function filter( list, predicate, self ) {
+				var visible = 0;
+				self.$dropMenu.find( '.empty-indicator' ).remove();
+
+				list.each( function( i ) {
+					var $li = $( this );
+					var text = $( this ).text().trim();
+
+					$li.removeClass();
+
+					if ( text === predicate ) {
+						$li.addClass( 'text-success' );
+						visible++;
+					} else if ( text.substr( 0, predicate.length ) === predicate ) {
+						$li.addClass( 'text-info' );
+						visible++;
+					} else {
+						$li.addClass( 'hidden' );
+					}
+				} );
+
+				if ( visible === 0 ) {
+					self.$dropMenu.append( '<li class="empty-indicator text-muted"><em>No Matches</em></li>' );
+				}
+			}
 		};
 
 		$.fn.combobox.Constructor = Combobox;
