3d08b52579628077b2eadb3b9968385c507a307f
swilliams <swilliams@exacttarget.com>
2016-05-03 23:31:39 -0400

    orphaned version updates

--- package.js
+++ package.js
@@ -6,7 +6,7 @@
 Package.describe({
   name: 'exacttarget:fuelux',  // http://atmospherejs.com/exacttarget/fuelux
   summary: 'Base Fuel UX styles and controls',
-  version: '3.15.1',
+  version: '3.15.2',
   git: 'https://github.com/ExactTarget/fuelux.git'
 });
 
