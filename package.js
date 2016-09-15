d2a46ebdfa2c5920b8fc7a6bd98e7d9c3cf9f6e3
Christopher McCulloh <cmcculloh@exacttarget.com>
2016-09-14 21:44:10 -0400

    cleaning things up in preparation for 3.15.7 release

--- package.js
+++ package.js
@@ -6,7 +6,7 @@
 Package.describe({
   name: 'exacttarget:fuelux',  // http://atmospherejs.com/exacttarget/fuelux
   summary: 'Base Fuel UX styles and controls',
-  version: '3.15.2',
+  version: '3.15.6',
   git: 'https://github.com/ExactTarget/fuelux.git'
 });
 
