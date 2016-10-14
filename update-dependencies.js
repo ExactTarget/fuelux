888d816e77a2c8d04bfad8556136f1d469221d02
Christopher McCulloh <cmcculloh@exacttarget.com>
2016-10-14 16:37:44 -0400

    adds check to make sure branch is clean. Also updates bower deps, as well as lightning-theme deps. Re-orders projects for correctness

--- update-dependencies.js
+++ update-dependencies.js
@@ -8,39 +8,86 @@ console.log('node version: ', version.stdout);
 
 const installBower = exec('npm install -g bower');
 
-
-const updateDependencyRequirements = (project, release) => {
-	console.log('updateDependencyRequirements', project);
+const prepBranch = (project, release, lightningThemeRelease) => {
 	return new Promise((resolve, reject) => {
 		const execOpts = {
-			silent: false,
+			silent: true,
 			async: false,
 			shell: '/bin/bash'
 		};
 
-		const command = String.raw`cd ../${project} && sed -i '' 's/\(fuelux": "\)[\^\>\=\~\<0-9.x]*\("\)/fuelux": "${release}"/g' ./package.json`;
+		const cd = `cd ../${project}`
+		const gitstuff = `git stash && git checkout master && git pull upstream master && git status`
+
+		const command = String.raw`${cd} && ${gitstuff}`;
 
 		// npm & bower update
 		const runUpdates = exec(command,
 			execOpts,
 			(code, stdout, stderr) => {
-				if (code === 0) {
-					console.log(project, 'updateDependencyRequirements successful');
-					resolve(project, release);
+				// check to see if status is clean
+				// console.log(stdout.match('nothing to commit, working directory clean'));// grep instead
+				// console.log(stdout);
+				if (code === 0 && stdout.match('nothing to commit, working directory clean').length > 0) {
+					// console.log(project, 'updateDependencyRequirements successful');
+					resolve(project, release, lightningThemeRelease);
 				} else {
 					console.log('Exit code:', code);
 					console.log(project, 'had errors when updateDependencyRequirements.')
 					console.log('Program output:', stdout);
 					console.log('Program stderr:', stderr);
-					reject(project, release);
+					reject(project, release, lightningThemeRelease);
 				}
 			}
 		);
 	});
 }
 
-const runUpdate = (project, release) => {
-	console.log('runUpdate', project, release);
+const updateDependencyRequirements = (project, release, lightningThemeRelease) => {
+	// console.log('updateDependencyRequirements', project);
+	return new Promise((resolve, reject) => {
+		const execOpts = {
+			silent: true,
+			async: false,
+			shell: '/bin/bash'
+		};
+
+		const cd = `cd ../${project}`
+
+		const packageFUX = String.raw`sed -i '' 's/\(fuelux": "\)[\^\>\=\~\<0-9.x]*\("\)/fuelux": "${release}"/g' ./package.json`;
+		const bowerFUX = String.raw`sed -i '' 's/\(fuelux": "\)[\^\>\=\~\<0-9.x]*\("\)/fuelux": "${release}"/g' ./bower.json`;
+
+		const packageFLTPrefix = String.raw`"fuelux-lightning-theme": "git+ssh:\/\/git@github.exacttarget.com:uxarchitecture\/fuelux-lightning-theme.git\#`;
+		const packageLightning = String.raw`sed -i '' 's/\(${packageFLTPrefix}\)[\^\>\=\~\<0-9.x]*\("\)/${packageFLTPrefix}${lightningThemeRelease}"/g' ./package.json`;
+
+		const bowerFLTPrefix = String.raw`"fuelux-lightning-theme": "git@github.exacttarget.com:uxarchitecture\/fuelux-lightning-theme.git\#`;
+		const bowerLightning = String.raw`sed -i '' 's/\(${bowerFLTPrefix}\)[\^\>\=\~\<0-9.x]*\("\)/${bowerFLTPrefix}${lightningThemeRelease}"/g' ./bower.json`;
+
+		const bowerResolutions = String.raw`sed -i '' 's/\(fuelux-lightning-theme": "\)[\^\>\=\~\<0-9.x]*\("\)/fuelux-lightning-theme": "${lightningThemeRelease}"/g' ./bower.json`;
+
+		const command = String.raw`${cd} && if [ -e package.json ]; then ${packageFUX} && ${packageLightning}; fi && if [ -e bower.json ]; then ${bowerFUX} && ${bowerLightning} && ${bowerResolutions}; fi`;
+
+		// npm & bower update
+		const runUpdates = exec(command,
+			execOpts,
+			(code, stdout, stderr) => {
+				if (code === 0) {
+					// console.log(project, 'updateDependencyRequirements successful');
+					resolve(project, release, lightningThemeRelease);
+				} else {
+					console.log('Exit code:', code);
+					console.log(project, 'had errors when updateDependencyRequirements.')
+					console.log('Program output:', stdout);
+					console.log('Program stderr:', stderr);
+					reject(project, release, lightningThemeRelease);
+				}
+			}
+		);
+	});
+}
+
+const runUpdate = (project, release, lightningThemeRelease) => {
+	// console.log('runUpdate', project, release, lightningThemeRelease);
 	return new Promise((resolve, reject) => {
 		// cd into library
 		const execOpts = {
@@ -54,22 +101,22 @@ const runUpdate = (project, release) => {
 			execOpts,
 			(code, stdout, stderr) => {
 				if (code === 0) {
-					console.log(project, 'runUpdate successful');
-					resolve(project, release);
+					// console.log(project, 'runUpdate successful');
+					resolve(project, release, lightningThemeRelease);
 				} else {
 					console.log('Exit code:', code);
 					console.log(project, 'had errors when runUpdating.')
 					console.log('Program output:', stdout);
 					console.log('Program stderr:', stderr);
-					reject(project, release);
+					reject(project, release, lightningThemeRelease);
 				}
 			}
 		);
 	});
 }
 
-const commitAndPush = (project, release) => {
-	console.log('runUpdate', project, release);
+const commitAndPush = (project, release, lightningThemeRelease) => {
+	// console.log('runUpdate', project, release, lightningThemeRelease);
 	return new Promise((resolve, reject) => {
 		// cd into library
 		const execOpts = {
@@ -79,12 +126,14 @@ const commitAndPush = (project, release) => {
 		};
 
 		// npm & bower update
-		const runUpdates = exec(`cd ../${project} && git checkout master && git pull upstream master && git commit -am "Updates dependency for FuelUX to ${release}" && git push upstream master`,
+		const runUpdates = exec(`cd ../${project} && git add -A && git commit -m "Updates dependency for FuelUX to ${release}" && git push upstream master`,
 			execOpts,
 			(code, stdout, stderr) => {
-				if (code === 0 || (code === 1 && stderr.substr(0, 19) === "Already on 'master'")) {
-					console.log(project, 'runUpdate successful');
-					resolve(project, release);
+				const noError = code === 0 || stderr === "";
+				const onMasterError = code === 1 && stderr.substr(0, 19) === "Already on 'master'"
+				if (noError || onMasterError) {
+					// console.log(project, 'runUpdate successful');
+					resolve(project, release, lightningThemeRelease);
 				} else {
 					console.log('');
 					console.log('=========================================');
@@ -94,7 +143,7 @@ const commitAndPush = (project, release) => {
 					console.log('Program output:', stdout);
 					console.log('=========================================');
 					console.log('');
-					reject(project, release);
+					reject(project, release, lightningThemeRelease);
 				}
 			}
 		);
@@ -102,45 +151,53 @@ const commitAndPush = (project, release) => {
 }
 
 const updateFuelUX = (project) => {
-	console.log('now updating', project);
-	const release = '3.15.9';
+	const fueluxRelease = '3.15.8';
+	const lightningThemeRelease = '0.3.0';
 
 	return new Promise((resolve, reject) => {
-		updateDependencyRequirements(project, release)
+		console.log(`${project} updating START`);
+		prepBranch(project, fueluxRelease, lightningThemeRelease)
 			.then(() => {
-				return runUpdate(project, release);
+				return updateDependencyRequirements(project, fueluxRelease, lightningThemeRelease)
 			})
 			.then(() => {
-				return commitAndPush(project, release);
+				return runUpdate(project, fueluxRelease, lightningThemeRelease);
 			})
 			.then(() => {
-				console.log('done updating', project, release);
-				resolve(project, release);
+				return commitAndPush(project, fueluxRelease, lightningThemeRelease);
+			})
+			.then(() => {
+				console.log(`${project} updating COMPLETE`);
+				resolve(project, fueluxRelease, lightningThemeRelease);
 			});
 	});
 };
+
+
 
 const libraryGroups = [
 	[
 		'fuelux-lightning-theme'
 	],
 	[
-		'fuel-site',
-		'fusion-fuel',
-		'components/data-mapper'
+		'fusion-fuel'
 	],
 	[
+		'fuelux-site',
 		'components/data-library',
-		'components/expression-builder'
+		'components/expression-text'
 	],
 	[
-		'components/expression-text',
+		'components/data-mapper',
+	],
+	[
+		'components/expression-builder',
 		'components/enlightenment',
 		'components/ftux',
 		'components/campaignselector'
 	],
 	[
-		'components'
+		'components/fuelux-components'
 	]
 ];
 
