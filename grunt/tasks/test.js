341d2f824dc2436c99bf20927a40c3f3364808bf
Stephen Williams <stephenwilliams@salesforce.com>
2016-12-09 13:49:18 -0500

    better commonjs test (clean dist, consistent test)
    - removed commonjs testing resource (js/npm.js) from dist folder
    - buld dist task no longer leaves dist/js/npm.js in zip
    - moved creation of resources for commonjs into prep test task
    - test dist task now preps for commonjs tasks and cleans up

--- grunt/tasks/test.js
+++ grunt/tasks/test.js
@@ -1,15 +1,21 @@
 module.exports = function test (grunt) {
 	// to be run prior to submitting a PR
 	grunt.registerTask('test', 'run jshint, qunit source w/ coverage, and validate HTML',
-		['browserify:commonjs', 'dist', 'jshint', 'connect:testServer', 'qunit:noMoment', 'qunit:globals', 'qunit:dist', 'htmllint', 'resetdist']);
+		['jshint', 'connect:testServer', 'qunit:noMoment', 'qunit:globals', 'test-dist', 'htmllint']);
+
+	grunt.registerTask('prep-commonjs-test', 'run commonjs config buiild and browserify to prep for commonjs test',
+		['commonjs', 'browserify:commonjs']);
+
+	grunt.registerTask('test-dist', 'run jshint, qunit source w/ coverage, and validate HTML',
+		['dist', 'prep-commonjs-test', 'qunit:dist', 'clean:commonjs']);
 
 	// If qunit:source is working but qunit:full is breaking, check to see if the dist broke the code. This would be especially useful if we start mangling our code, but, is 99.99% unlikely right now
 	grunt.registerTask('validate-dist', 'run qunit:source, dist, and then qunit:full',
-		['connect:testServer', 'qunit:source', 'dist', 'browserify:commonjs', 'qunit:dist']);
+		['connect:testServer', 'qunit:source', 'test-dist']);
 
 	// multiple jQuery versions, then run SauceLabs VMs
 	grunt.registerTask('releasetest', 'run jshint, build dist, all source tests, validation, and qunit on SauceLabs',
-		['test', 'dist', 'browserify:commonjs', 'qunit:dist', 'saucelabs-qunit:defaultBrowsers']);
+		['test', 'saucelabs-qunit:defaultBrowsers']);
 
 	// Due to TravisCI security concerns, Saucelabs can not be run for PRs from forks. Therefore, it can not become part of our PR process.
 	// https://docs.travis-ci.com/user/pull-requests/#Pull-Requests-and-Security-Restrictions
@@ -18,7 +24,7 @@ module.exports = function test (grunt) {
 		['connect:testServer', 'jshint', 'saucelabs-qunit:defaultBrowsers']);
 
 	grunt.registerTask('travisci', 'Tests to run when in Travis CI environment',
-		['browserify:commonjs', 'test', 'dist', 'qunit:dist']);
+		['test']);
 
 	// if you've already accidentally added your files for commit, this will at least unstage them. If you haven't, this will wipe them out.
 	grunt.registerTask('resetdist', 'resets changes to dist to keep them from being checked in', function resetdist () {
