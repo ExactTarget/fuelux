1286b571ea2e475e4fec28efac4af16a0037bc37
Christopher McCulloh <cmcculloh@exacttarget.com>
2017-05-23 15:51:22 -0400

    comment out other tests while developing

--- test/tree-test.js
+++ test/tree-test.js
@@ -24,20 +24,22 @@ define(function treeTest (require) {
 		assert.ok($tree.tree() === $tree, 'tree should be initialized');
 	});
 
-	QUnit.test('should have correct defaults', require('./tree-tests/defaults'));
-	QUnit.test('should call dataSource correctly', require('./tree-tests/calls-datasource'));
-	QUnit.test('Tree should be populated by items on initialization', require('./tree-tests/is-populated'));
-	QUnit.test('Folder should populate when opened', require('./tree-tests/does-folder-populate'));
-	QUnit.test('getValue alias should function', require('./tree-tests/get-value-alias'));
-	QUnit.test('Single item/folder selection works as designed', require('./tree-tests/single-select'));
-	QUnit.test('Multiple item/folder selection works as designed', require('./tree-tests/multi-select'));
-	QUnit.test('should not allow selecting items if disabled', require('./tree-tests/disable-select-items'));
-	QUnit.test('should not allow selecting folders if disabled', require('./tree-tests/disable-select-folders'));
-	QUnit.test('folders should open and close correctly', require('./tree-tests/folders-open-close'));
-	QUnit.test('should disclose visible folders', require('./tree-tests/disclose-visible'));
-	QUnit.test('should disclose all folders up to limit, and then close them, then open them all', require('./tree-tests/disclose-all'));
-	QUnit.test('should refresh an already opened/cached folder with new nodes', require('./tree-tests/refresh'));
-	QUnit.test('Tree should accept TEXT as the NAME property in the datasource', require('./tree-tests/tree-accepts-name'));
+	// QUnit.test('should have correct defaults', require('./tree-tests/defaults'));
+	// QUnit.test('should call dataSource correctly', require('./tree-tests/calls-datasource'));
+	// QUnit.test('should be populated by items on initialization', require('./tree-tests/is-populated'));
+	// QUnit.test('folder should populate when opened', require('./tree-tests/does-folder-populate'));
+	// QUnit.test('getValue alias should function', require('./tree-tests/get-value-alias'));
+	// QUnit.test('single item/folder selection works as designed', require('./tree-tests/single-select'));
+	// QUnit.test('multiple item/folder selection works as designed', require('./tree-tests/multi-select'));
+	// QUnit.test('should not allow selecting items if disabled', require('./tree-tests/disable-select-items'));
+	// QUnit.test('should not allow selecting folders if disabled', require('./tree-tests/disable-select-folders'));
+	// QUnit.test('folders should open and close correctly', require('./tree-tests/folders-open-close'));
+	// QUnit.test('should disclose visible folders', require('./tree-tests/disclose-visible'));
+	// QUnit.test('should disclose all folders up to limit, and then close them, then open them all', require('./tree-tests/disclose-all'));
+	// QUnit.test('should refresh an already opened/cached folder with new nodes', require('./tree-tests/refresh'));
+	// QUnit.test('should accept TEXT as the NAME property in the datasource', require('./tree-tests/tree-accepts-name'));
+	// QUnit.test('should have proper tabindexes', require('./tree-tests/tab-indexes'));
+	QUnit.test('should respond to arrow keys', require('./tree-tests/responds-to-arrow-keys'));
 
 	QUnit.test('should destroy control', function destroyWorks (assert) {
 		var $tree = $(html).find('#MyTree');
