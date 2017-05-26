378b08e7a2d22712d0c25bd3f316023e89a7d38b
Christopher McCulloh <cmcculloh@exacttarget.com>
2017-05-26 09:03:23 -0400

    test focus on tree with no selected element. Tests tab index for focused element on focusIn

--- test/tree-tests/focus-module.js
+++ test/tree-tests/focus-module.js
@@ -1,30 +1,60 @@
-define(function respondsToKeyboardInputModule (require) {
+define(function focusModule (require) {
 	var $ = require('jquery');
 
 	return function (QUnit) {
 		QUnit.module( 'focusIn', {}, function () {
-			QUnit.test('should focus on first focusable branch when nothing is selected', function respondsToKeyboardInput (assert) {
+			QUnit.test('should focus on first focusable branch when nothing is selected', function checkFocusOnNonSelect (assert) {
+				assert.expect( 1 );
 				var $tree = this.$tree;
 
-				// this.$tree.tree('discloseVisible');
+				$tree.on('loaded.fu.tree', function () {
+					$tree.on('focus', function () {
+						var $focused = $(document.activeElement);
+						var $firstFocusableChild = $(document.activeElement);
+						assert.equal($focused.attr('id'), $firstFocusableChild.attr('id'), 'first focusable branch is focused on');
+					});
+
+					var tree = document.getElementById('MyTree');
+
+					var event = new Event('focus');
+
+					tree.dispatchEvent(event);
+				});
+
 				$tree.tree({
 					dataSource: this.dataSource
 				});
-
-				setTimeout(function() {
-					$tree.focus();
-				}, 1000);
-
-				// console.log('this.$tree', this.$tree.parent().html())
-
-				assert.ok(false, 'test');
 			});
 
-			QUnit.test('should focus on first selected item when there is a selection', function respondsToKeyboardInput (assert) {
-				this.$tree.trigger(this.getKeyDown('left'));
+			QUnit.test('should correctly set tabindex', function testTabIndex (assert) {
+				assert.expect( 2 );
+				var $tree = this.$tree;
 
-				assert.ok(true, 'test');
+				$tree.on('loaded.fu.tree', function () {
+					var $focused = $($tree.find('li:not(".hidden"):first')[0]);
+					assert.equal($focused.attr('tabindex'), undefined, 'tabindex defaults to undefined');
+
+					$tree.on('focus', function () {
+						assert.equal($focused.attr('tabindex'), '0', 'tabindex set to 0');
+					});
+
+					var tree = document.getElementById('MyTree');
+
+					var event = new Event('focus');
+
+					tree.dispatchEvent(event);
+				});
+
+				$tree.tree({
+					dataSource: this.dataSource
+				});
 			});
+
+			// QUnit.test('should focus on first selected item when there is a selection', function respondsToKeyboardInput (assert) {
+			// 	this.$tree.trigger(this.getKeyDown('left'));
+
+			// 	assert.ok(true, 'test');
+			// });
 		});
 	};
 });
