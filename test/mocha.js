c8fc98b47f009745a9174e47a9bf34b085917c25
Christopher McCulloh <cmcculloh@exacttarget.com>
2017-06-09 15:03:04 -0400

    (GH1986) remove generator to try and fix travis ci

--- test/mocha.js
+++ test/mocha.js
@@ -1,16 +1,16 @@
 /* global describe:false, it:false, before:false, after:false */
 
 /* mocha tests for Niffy go here */
-var express = require('express');
-var exphbs  = require('express-handlebars');
-var path = require('path');
+// var express = require('express');
+// var exphbs  = require('express-handlebars');
+// var path = require('path');
 
 var server = require('./regression/server.js');
 
-var should = require('chai').should();
+// var should = require('chai').should();
 var Niffy = require('niffy');
 
-describe('Fuel UX 3', function () {
+describe('Fuel UX 3', function testFuelUX3 () {
 	var basehost = 'http://localhost:8000';
 	var testhost = 'http://localhost:8013';
 	var niffy;
@@ -20,19 +20,19 @@ describe('Fuel UX 3', function () {
 	before(function instantiateNiffy () {
 		devServer = server.getServer(8000);
 		refServer = server.getServer(8013);
-		niffy = new Niffy(basehost, testhost, { show: false, threshold: 0 });
+		niffy = new Niffy(basehost, testhost, { show: true, threshold: 0 });
 	});
-	after(function* () {
-		yield niffy.end();
+	after(function stopAll () {
+		niffy.end();
 		devServer.close();
 		refServer.close();
 	});
 
-	describe('Components', function () {
+	describe('Components', function testComponents () {
 		/**
 		 * Top level.
 		 */
-		it('/checkbox', function* () {
+		it('/checkbox', function* testCheckbox () {
 			yield niffy.test('/checkbox');
 		});
 	});
