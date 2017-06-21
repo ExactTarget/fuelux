e97f9c42736ec7d2f9d8060b6b74eff386ef57ab
Christopher McCulloh <cmcculloh@exacttarget.com>
2017-06-21 11:51:35 -0400

    (CH1972) adds unit test for ignoreVisibilityCheck option

--- test/checkbox-tests/visibility.js
+++ test/checkbox-tests/visibility.js
@@ -24,6 +24,20 @@ define(function checkboxVisibilityModule () {
 			$checkbox.checkbox();
 		});
 
+		QUnit.test('should not error on init if checkbox visibility hidden and ignoreVisibilityCheck option passed', function testIgnoreError (assert) {
+			assert.expect(1);
+
+			window.console.error = function error(msg) {
+				assert.notOk(msg, 'console.error should not have fired');
+			};
+
+			var $checkbox = this.$fixture.find('#CheckboxHidden');
+			var $input = $checkbox.find('input[type="checkbox"]');
+
+			assert.equal($input.css('visibility'), 'hidden', 'visibility set to hidden');
+			$checkbox.checkbox({ignoreVisibilityCheck: true});
+		});
+
 		QUnit.test('should error on init if checkbox visibility hidden', function testHiddenError (assert) {
 			assert.expect(2);
 
