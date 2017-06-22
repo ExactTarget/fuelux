2f7d4c7390943c51452dadb617b8975cbcde9314
Christopher McCulloh <cmcculloh@exacttarget.com>
2017-06-22 13:29:03 -0400

    (GH1994) tests down arrow press between siblings

--- test/tree-tests/down-key-module.js
+++ test/tree-tests/down-key-module.js
@@ -3,8 +3,29 @@ define(function keyboardNavigationModuleFactory (require) {
 
 	return function downKeyModule (QUnit) {
 		QUnit.module( 'should respond to down key', {}, function testDownKeyPresses () {
-			QUnit.skip('when focus is on node above sibling node, moves focus down to sibling', function loadTree (assert) {
+			QUnit.test('when focus is on node above sibling node, moves focus down to sibling', function loadTree (assert) {
+				assert.expect(2);
 
+				this.$tree.on('populated.fu.tree', function triggerDownArrow () {
+					var $initialBranch = $(this.$tree.find('li:not(".hidden"):first'));
+					var $nextBranch = $(this.$tree.find('li:not(".hidden")').get(1));
+
+					$initialBranch.attr('tabindex', 0);
+					$initialBranch.focus();
+
+					this.$tree.on('keyboardNavigated.fu.tree', function testDownArrowResult () {
+						assert.equal($(document.activeElement).attr('id'), $nextBranch.attr('id'), 'next sibling now has focus');
+					});
+
+					assert.equal($(document.activeElement).attr('id'), $initialBranch.attr('id'), 'initial branch has focus');
+
+					var pressDownArrow = this.getKeyDown('down', $initialBranch);
+					$initialBranch.trigger(pressDownArrow);
+				}.bind(this));
+
+				this.$tree.tree({
+					dataSource: this.dataSource
+				});
 			});
 
 			QUnit.skip('when focus is on last focusable child of parent, moves focus out of parent onto first focusable sibling of parent', function loadTree (assert) {
