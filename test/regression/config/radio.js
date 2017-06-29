0c8c285b3bc6a52648b7b6cf8ba5fcedd1dbfc1a
Christopher McCulloh <cmcculloh@exacttarget.com>
2017-06-29 17:30:27 -0400

    separates radios into two groups in regression test so that checked enabled shows as checked

--- test/regression/config/radio.js
+++ test/regression/config/radio.js
@@ -4,9 +4,14 @@ module.exports = {
 			id: 'myRadios',
 			radios: [
 				{label: 'Custom radio unchecked on pageload'},
-				{checked:true, label: 'Custom radio checked on pageload'},
-				{disabled:true, label: 'Custom radio disabled on pageload'},
-				{checked:true, disabled:true, label: 'Custom radio checked and disable on pageload'}
+				{checked: true, label: 'Custom radio checked on pageload'}
+			]
+		},
+		{
+			id: 'myRadios2',
+			radios: [
+				{disabled: true, label: 'Custom radio disabled on pageload'},
+				{checked: true, disabled: true, label: 'Custom radio checked and disable on pageload'}
 			]
 		}
 	]
