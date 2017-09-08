c0850a9e29e8042fc7c27eca53185ee6b89b6530
jacob-roschen <jacob.roschen@salesforce.com>
2017-09-08 16:09:06 -0400

    Add a test that demonstrates how the tree consumes all space, enter, up, down, left, right, home, and end keydown events

--- test/tree-tests/space-key-module.js
+++ test/tree-tests/space-key-module.js
@@ -15,5 +15,34 @@ define(function keyboardNavigationModuleFactory (require) {
 				// Skipped due to time constraints. If you touch this part of the tree, please complete this test
 			});
 		});
+
+		QUnit.module( 'should not respond to space key', {}, function testSpaceKeyPresses () {
+			QUnit.test('when focus is on a child element', function loadTree (assert) {
+				assert.expect(1);
+
+				this.$tree.on('initialized.fu.tree', function triggerDownArrow () {
+					var $initialBranch = $(this.$tree.find('li:not(".hidden")').get(1));
+
+					$initialBranch.attr('tabindex', 0);
+					$initialBranch.focus();
+
+					this.$tree.on('keyboardNavigated.fu.tree', function testDownArrowResult () {
+						assert.notOk(true, 'the keyboardNavigated event should not be triggered');
+					});
+
+					assert.equal($(document.activeElement).attr('id'), $initialBranch.attr('id'), 'initial branch has focus');
+
+					var $popoverDiv = $('<div class="popover" />');
+					$initialBranch.append($popoverDiv);
+
+					var pressSpaceKey = this.getKeyDown('down', $popoverDiv);
+					$popoverDiv.trigger(pressSpaceKey);
+				}.bind(this));
+
+				this.$tree.tree({
+					dataSource: this.dataSource
+				});
+			});
+		});
 	};
 });
